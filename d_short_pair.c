71aabd318d556cf7b2f14220863e470ebd301823
kouamano <amano.au1@gmail.con>
2014-02-26 13:53:29 +0900

    add

--- d_short_pair.c
+++ d_short_pair.c
@@ -1,29 +1,39 @@
+/* d_short_pair.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/alloc.c"
 #define LEN 1024
+
+#include "edgeop.h"
 
 struct options {
 	int help;
 	int stat;
 	int check;
-	int argint;
-	char *argstr;
+	char *df;
+	char *ef;
+	int dsize;
+	int p;
+	int t;
 };
 
 void help(void){
 	printf("USAGE:\n");
-	printf(" template [-h] [-s] [-c] int=<argint> str=<argstr>.\n");
+	printf(" template [-h] [-s] [-c] df=<dist mat file> dsize=<mat size> p=<primal node> t=<terminanode> ef=<edge file> .\n");
 	printf("  -h : help.\n");
 	printf("  -s : stat.\n");
 	printf("  -c : check args.\n");
-	printf("  argint : set integer.\n");
-	printf("  argstr : set *chars (len < 1024).\n");
+	printf("  dist mat : integer matrix.\n");
+	printf("  mat size : integer.\n");
+	printf("  primal node : integer.\n");
+	printf("  terminal node : integer.\n");
+	printf("  edge file : list of node vs node.\n");
 }
 
 void status(void){
 	printf("STATUS:\n");
-	printf(" this program is template.\n");
+	printf(" under construction.\n");
 }
 
 struct options *alloc_options(void){
@@ -32,7 +42,11 @@ struct options *alloc_options(void){
 		printf("failed : malloc() in alloc_options().\n");
 		exit(1);
 	}
-	if(((*p).argstr = malloc(sizeof(char) * LEN)) == NULL){
+	if(((*p).df = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).ef = malloc(sizeof(char) * LEN)) == NULL){
 		printf("failed : malloc() in alloc_options().\n");
 		exit(1);
 	}
@@ -43,8 +57,11 @@ void init_options(struct options *opt){
 	(*opt).help = 0;
 	(*opt).stat = 0;
 	(*opt).check = 0;
-	(*opt).argint = 0;
-	(*opt).argstr[0] = '\0';
+	(*opt).df[0] = '\0';
+	(*opt).ef[0] = '\0';
+	(*opt).dsize = 0;
+	(*opt).p = 0;
+	(*opt).t = 0;
 }
 
 void get_options(int optc, char **optv, struct options *opt){
@@ -56,23 +73,44 @@ void get_options(int optc, char **optv, struct options *opt){
 			(*opt).stat = 1;
 		}else if(strcmp(optv[i],"-c") == 0){
 			(*opt).check = 1;
-		}else if(strncmp(optv[i],"int=",4) == 0){
-			sscanf(optv[i],"int=%d",&(*opt).argint);
-		}else if(strncmp(optv[i],"str=",4) == 0){
-			sscanf(optv[i],"str=%s",(*opt).argstr);
+		}else if(strncmp(optv[i],"dsize=",6) == 0){
+			sscanf(optv[i],"dsize=%d",&(*opt).dsize);
+		}else if(strncmp(optv[i],"df=",3) == 0){
+			sscanf(optv[i],"df=%s",(*opt).df);
+		}else if(strncmp(optv[i],"ef=",3) == 0){
+			sscanf(optv[i],"ef=%s",(*opt).ef);
+		}else if(strncmp(optv[i],"p=",2) == 0){
+			sscanf(optv[i],"p=%d",&(*opt).p);
+		}else if(strncmp(optv[i],"t=",2) == 0){
+			sscanf(optv[i],"t=%d",&(*opt).t);
+
 		}
 	}
 }
 
 void check_options(struct options *opt){
 	printf("OPTIONS:\n");
-	printf(" opt.argint:%d:\n",(*opt).argint);
-	printf(" opt.argstr:%s:\n",(*opt).argstr);
+	printf(" opt.df:%s:\n",(*opt).df);
+	printf(" opt.ef:%s:\n",(*opt).ef);
+	printf(" opt.dsize:%d:\n",(*opt).dsize);
+	printf(" opt.p:%d:\n",(*opt).p);
+	printf(" opt.t:%d:\n",(*opt).t);
 }
 
 int main(int argc, char **argv){
 	struct options *opt;
 	int ie=0;
+	FILE *fp;
+	char *line;
+	int c;
+	int num_RNG_edge = 0;
+	struct edge RNG_edge;
+	struct RNG_table RNG_tbl;
+
+	int i,j,k,count;
+	int *tmp_i_vec;
+
+	/* (* check options */
 	opt = alloc_options();
 	init_options(opt);
 	get_options(argc-1, argv+1, opt);
@@ -94,5 +132,66 @@ int main(int argc, char **argv){
 	if(ie == 1){
 		exit(0);
 	}
+	/* *) */
+
+	/* (* read RNG */
+	line = c_alloc_vec(LEN);
+	if((fp = fopen((*opt).ef,"r")) == NULL){
+		perror((*opt).ef);
+		exit(1);
+	}
+	num_RNG_edge = 0;
+	while((c=fgetc(fp)) != EOF){
+		if((char)c == '\n'){
+			num_RNG_edge++;
+		}
+	}
+	printf("num_RNG_edge:%d:\n",num_RNG_edge);
+	fseek(fp,0U,SEEK_SET);
+	RNG_edge.p = i_alloc_vec(num_RNG_edge * 2);
+	RNG_edge.t = i_alloc_vec(num_RNG_edge * 2);
+	i = 0;
+	while((fgets(line,LEN,fp)) != NULL){
+		sscanf(line,"%d,%d",RNG_edge.p+i,RNG_edge.t+i);
+	i++;
+	}
+	fclose(fp);
+	for(j=i;j<num_RNG_edge*2;j++){
+		RNG_edge.t[j] = RNG_edge.p[j-num_RNG_edge];
+		RNG_edge.p[j] = RNG_edge.t[j-num_RNG_edge];
+	}
+	/* *) */
+
+	/* (* create RNG_tbl */
+	tmp_i_vec = i_alloc_vec((*opt).dsize);
+	RNG_tbl.size = i_alloc_vec((*opt).dsize);
+	RNG_tbl.next_pos = pi_alloc_vec((*opt).dsize);
+	for(i=0;i<(*opt).dsize;i++){
+		//printf("i:%d:\n",i);
+		count = 0;
+		for(j=0;j<num_RNG_edge*2;j++){
+			//printf(" j:%d:\n",j);
+			if(i == RNG_edge.p[j]){
+				tmp_i_vec[count] = RNG_edge.t[j];
+				count++;
+			}
+		}
+		RNG_tbl.size[i] = count;
+		RNG_tbl.next_pos[i] = i_alloc_vec(RNG_tbl.size[i]);
+		for(k=0;k<RNG_tbl.size[i];k++){
+			RNG_tbl.next_pos[i][k] = tmp_i_vec[k];
+		}
+	}
+	printf("RNG_tbl:\n");
+	for(i=0;i<(*opt).dsize;i++){
+		printf("[%d]",i);
+		for(k=0;k<RNG_tbl.size[i];k++){
+			printf(",%d",RNG_tbl.next_pos[i][k]);
+		}
+		printf("\n");
+	}
+	/* *) */
+
+
 	return(0);
 }
