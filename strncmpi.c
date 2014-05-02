a1183094f94b3a11a846323c1edd167f333cfcc1
kouamano <amano.au1@gmail.con>
2014-05-02 14:10:32 +0900

    rev

--- strncmpi.c
+++ strncmpi.c
@@ -5,6 +5,8 @@ int strncmpi(const char *s1, const char *s2, size_t n){
 			return(-1);
 		}else if(toupper(s1[i]) > toupper(s2[i])){
 			return(1);
+		}else if((s1[i] == 0) && (s2[i] == 0)){
+			return(0)
 		}else{
 			;
 		}
