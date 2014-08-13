8f008e61ae30504e6c55e440805a9c6caeadae75
kouamano <amano.au1@gmail.com>
2014-08-13 15:25:05 +0900

    rev

--- sum_count_from_mixed_sort.pl
+++ sum_count_from_mixed_sort.pl
@@ -2,18 +2,24 @@
 
 while(<>){
 	chomp;
-	$_ =~ s/\s/ /ig;
+	$_ =~ s/\s/\t/ig;
 	$_ =~ s/^\s//;
 	$_ =~ s/\s$//;
+	$_ =~ s/\t/ /;
 	push(@arr,$_);
 }
-($fn,$ft) = split(' ',$arr[0]);
+#print "=====\n";
+($fn,$ft) = split(/ /,$arr[0]);
+#print ";;;$fn;;;";
+#print ";;;$ft;;;";
+#print "=====\n";
 $former = $arr[0];
 $sum = $fn;
 shift(@arr);
 foreach (@arr){
-	($fn,$ft) = split(' ',$former);
-	($cn,$ct) = split(' ',$_);
+	($fn,$ft,$fk) = split(/ /,$former);
+	#print ";;; $ft ;;; $fk ;;;\n";
+	($cn,$ct) = split(/ /,$_);
 	if($ft eq $ct){
 		$sum = $sum + $cn;
 	}else{
