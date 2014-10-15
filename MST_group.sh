7ca60c5d68d9b8aaf9c5568ea2b7df99df0efe42
KOUAMANO <kouamano@dhcp10-216.slis.tsukuba.ac.jp>
2014-10-15 19:15:59 +0900

    rev

--- MST_group.sh
+++ MST_group.sh
@@ -3,7 +3,7 @@
 file_head=$1
 num_files=$2
 
-for n in `seq 1 $num_files`;
+for n in `seq $num_files`;
 	do echo ${file_head}-${n}.dmat;
 	MST dmat=${file_head}-${n}.dmat > ${file_head}-${n}.MST
 done
