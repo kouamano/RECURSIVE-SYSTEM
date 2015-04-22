b3827fc384b8a6fc903f07676eea94d76ebd2d3c
kouamano <amano.au1@gmail.com>
2015-04-22 12:39:56 +0900

    new

---
+++ match_substr.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use List::Util qw(max);
+
+$sourcefile = $ARGV[0];
+$substrfile = $ARGV[1];
+
+open(IN,$sourcefile);
+while(<IN>){
+	chomp;
+	push(@source,$_);
+}
+close(IN);
+
+open(IN,$substrfile);
+while(<IN>){
+	chomp;
+	push(@substr,$_);
+}
+close(IN);
+
+foreach(@source){
+	$str = $_;
+	foreach(@substr){
+		if($src =~ /$_/){
+			print "$src\n"
+			break;
+		}
+	}
+}
