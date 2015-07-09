cfd4af149b72d0540cb55d401d52b0d6bd063dbd
kouamano <amano.au1@gmail.com>
2015-07-09 16:34:20 +0900

    rev

--- split_RCBx.pl
+++ split_RCBx.pl
@@ -4,10 +4,10 @@ while(<>){
 	chomp;
 	if($_ =~ /\(RCB[0-9]+\)/){
 		#print "$_\n";
-		@tmp = split(/[\(\)]/,$_);
-		foreach(@tmp){
-			print "$_\n";
-		}
+		$_ =~ s/(\(RCB[0-9]+\))/ $1 /g;
+		$_ =~ s/ \(/\n/g;
+		$_ =~ s/\) /\n/g;
+		print "$_\n";
 	}else{
 		print "$_\n";
 	}
