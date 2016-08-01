3bd16e65bcc218a8aba1a5c9651d2c2a78027d7d
kouamano <amano.au1@gmail.com>
2016-08-01 17:29:24 +0900

    new

---
+++ split_phrase5W.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+while(<>){
+	chomp;
+	@word = split(/\s/,$_);
+	if($word[0] == ""){
+		shift(@word);
+	}
+	for($i=0; $i<$#word; $i++){
+		print "$word[$i] $word[$i+1] $word[$i+2] $word[$i+3] $word[$i+4]\n";
+	}
+}
