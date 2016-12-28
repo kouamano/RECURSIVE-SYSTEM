887b79e4b3a3e551ec5b3dd4780cc96a277a7dc5
kouamano <amano.au1@gmail.com>
2016-12-28 09:49:21 +0900

    edd

--- macpadding.pl
+++ macpadding.pl
@@ -1,19 +1,38 @@
 #!/usr/bin/perl
 
+sub _padding{
+	@sarr = split(/:/,$_[0]);
+	@oarr = ();
+	foreach(@sarr){
+		if(length($_) > 1){
+			$n = $_;
+		}else{
+			$n = "0"."$_";
+		}
+		push(@oarr,$n);
+	}
+	return join(":",@oarr);
+}
+
 while(<>){
 	chomp;
-	if($_ =~ / ([0-9a-fA-F]+:)+[0-9a-fA-F]+$/){
+	if($_ =~ / ([0-9a-fA-F]+:){5}[0-9a-fA-F]+$/){
 		@line = split(/ /,$_);
 		$target = pop(@line);
 		$str = join(" ",@line);
-		@target = split(/:/,$target);
-		@out = ();
-		foreach(@target){
-			$n = sprintf("%02d",$_);
-			push(@out,$n);
-		}
-		$out = join(":",@out);
-		print "$str $out\n";
+#		@target = split(/:/,$target);
+#		@out = ();
+#		foreach(@target){
+#			if(length($_) > 1){
+#				$n = $_;
+#			}else{
+#				$n = "0"."$_";
+#			}
+#			push(@out,$n);
+#		}
+#		$out = join(":",@out);
+		$pout = &_padding($target);
+		print "$str $pout\n";
 	}else{
 		print "$_\n";
 	}
