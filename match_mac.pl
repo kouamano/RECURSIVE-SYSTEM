a5adf23501e405b565f6a5a095dd9e6f078332f3
kouamano <amano.au1@gmail.com>
2017-01-06 17:41:12 +0900

    add

--- match_mac.pl
+++ match_mac.pl
@@ -3,27 +3,29 @@
 $fip = $ARGV[0];
 $fuser = $ARGV[1];
 
-open(IN,$fip);
-%ip = ();
-while(<IN>){
-	chomp;
-	@arr = split(/\s/,$_);
-	$first = shift(@arr);
-	$last = pop(@arr);
-	#print "$first\n";
-	#print "$last\n";
-	$ip{$last} = $first;
-}
-close(IN);
-
-#print %ip;
-
+%machash = ();
 open(IN,$fuser);
 while(<IN>){
 	chomp;
-	@arr = split(/\s/,$_);
-	$mac = shift(@arr);
-	$usr = pop(@arr);
-	print "$mac\t$usr\t$ip{$mac}\n";
+	$_ =~ s/^\s+//;
+	$_ =~ s/\s+$//;
+	$_ =~ s/\s+/\t/g;
+	@arr = split(/\t/,$_);
+	$machash{$arr[0]} = $arr[1];
 }
 close(IN);
+#print %machash;
+
+open(IN,$fip);
+while(<IN>){
+	chomp;
+	$_ =~ s/^\s+//g;
+	$_ =~ s/\s+$//g;
+	$_ =~ s/\s+/\t/g;
+	@line = split(/\t/,$_);
+	print "$line[0]\t$line[2]\t";
+	print $machash{$line[2]};
+	print "\n";
+}
+close(IN);
+
