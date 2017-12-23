4a4ae50edfdfe64f6c31567677b2974794cfa629
kouamano <amano.au1@gmail.com>
2017-12-23 15:03:17 +0900

    add

--- check_size.c
+++ check_size.c
@@ -6,9 +6,13 @@ int main(){
 	printf("_Bool           \t:%ld:\tbyte\n",sizeof(_Bool));
 	printf("short int       \t:%ld:\tbyte\n",sizeof(short int));
 	printf("int             \t:%ld:\tbyte\n",sizeof(int));
+	  int a[2][2];
+	printf("int 2D:2x2      \t:%ld:\tbyte\n",sizeof(a));
 	printf("long int        \t:%ld:\tbyte\n",sizeof(long int));
 	printf("long long int   \t:%ld:\tbyte\n",sizeof(long long int));
 	printf("float           \t:%ld:\tbyte\n",sizeof(float));
+	  int b[2][2];
+	printf("float 2D:2x2    \t:%ld:\tbyte\n",sizeof(b));
 	printf("double          \t:%ld:\tbyte\n",sizeof(double));
 	printf("long double     \t:%ld:\tbyte\n",sizeof(long double));
 	printf("size_t          \t:%ld:\tbyte\n",sizeof(size_t));
