c04bf0ece1e0ce235746837714175b10444d358b
kouamano <amano.au1@gmail.com>
2018-08-21 14:09:12 +0900

    rev

--- check_size.c
+++ check_size.c
@@ -5,29 +5,31 @@
 #include <complex.h>
 
 int main(){
-	printf("char            \t:%ld:\tbyte\n",sizeof(char));
-	printf("_Bool           \t:%ld:\tbyte\n",sizeof(_Bool));
-	printf("mode_t          \t:%ld:\tbyte\n",sizeof(mode_t));
-	printf("short int       \t:%ld:\tbyte\n",sizeof(short int));
-	printf("int             \t:%ld:\tbyte\n",sizeof(int));
+	printf("char                \t:%ld:\tbyte\n",sizeof(char));
+	printf("_Bool               \t:%ld:\tbyte\n",sizeof(_Bool));
+	printf("mode_t              \t:%ld:\tbyte\n",sizeof(mode_t));
+	printf("short int           \t:%ld:\tbyte\n",sizeof(short int));
+	printf("int                 \t:%ld:\tbyte\n",sizeof(int));
 	  int a[2][2];
-	printf("int 2D:2x2      \t:%ld:\tbyte\n",sizeof(a));
-	printf("long int        \t:%ld:\tbyte\n",sizeof(long int));
-	printf("long long int   \t:%ld:\tbyte\n",sizeof(long long int));
-	printf("float           \t:%ld:\tbyte\n",sizeof(float));
+	printf("int 2D:2x2          \t:%ld:\tbyte\n",sizeof(a));
+	printf("long int            \t:%ld:\tbyte\n",sizeof(long int));
+	printf("long long int       \t:%ld:\tbyte\n",sizeof(long long int));
+	printf("float               \t:%ld:\tbyte\n",sizeof(float));
 	  float b[2][2];
-	printf("float 2D:2x2    \t:%ld:\tbyte\n",sizeof(b));
-	printf("double          \t:%ld:\tbyte\n",sizeof(double));
-	printf("long double     \t:%ld:\tbyte\n",sizeof(long double));
-	printf("_Complex        \t:%ld:\tbyte\n",sizeof(_Complex));
-	printf("complex         \t:%ld:\tbyte\n",sizeof(complex));
-	printf("double _Complex \t:%ld:\tbyte\n",sizeof(double _Complex));
-	printf("size_t          \t:%ld:\tbyte\n",sizeof(size_t));
-	printf("time_t          \t:%ld:\tbyte\n",sizeof(time_t));
-	printf("void            \t:%ld:\tbyte\n",sizeof(void));
-	printf("pointer char*   \t:%ld:\tbyte\n",sizeof(char*));
-	printf("pointer int*    \t:%ld:\tbyte\n",sizeof(int*));
-	printf("pointer double* \t:%ld:\tbyte\n",sizeof(double*));
-	printf("pointer void*   \t:%ld:\tbyte\n",sizeof(void*));
+	printf("float 2D:2x2        \t:%ld:\tbyte\n",sizeof(b));
+	printf("double              \t:%ld:\tbyte\n",sizeof(double));
+	printf("long double         \t:%ld:\tbyte\n",sizeof(long double));
+	printf("_Complex            \t:%ld:\tbyte\n",sizeof(_Complex));
+	printf("complex             \t:%ld:\tbyte\n",sizeof(complex));
+	printf("double _Complex     \t:%ld:\tbyte\n",sizeof(double _Complex));
+	  complex c;
+	printf("complex __real__    \t:%ld:\tbyte\n",sizeof(__real__ c));
+	printf("size_t              \t:%ld:\tbyte\n",sizeof(size_t));
+	printf("time_t              \t:%ld:\tbyte\n",sizeof(time_t));
+	printf("void                \t:%ld:\tbyte\n",sizeof(void));
+	printf("pointer char*       \t:%ld:\tbyte\n",sizeof(char*));
+	printf("pointer int*        \t:%ld:\tbyte\n",sizeof(int*));
+	printf("pointer double*     \t:%ld:\tbyte\n",sizeof(double*));
+	printf("pointer void*       \t:%ld:\tbyte\n",sizeof(void*));
 	return(0);
 }
