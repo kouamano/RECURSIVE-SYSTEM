030a199a136fd8113c46b979eb081976f103e71f
kouamano <amano.au1@gmail.com>
2018-11-26 11:47:00 +0900

    rev

--- list-tester.01.c
+++ list-tester.01.c
@@ -90,29 +90,10 @@ int main(void){
 	ExFunction_Recursive_S_Print(&precell, (struct List *(*)())Function_Print_Status,WL,NULL);
 	printf("\n");
 	printf("===== =====\n");
-	Function_Print_Index(&precell);
 	ExFunction_Recursive_List(&precell,Function_Print_Index);
 	printf("===== =====\n");
 	printf("=====\n");
 
-	printf("===== ex.05 =\n");
-	init_List_zero(&precell);
-	cell = malloc(sizeof(struct List) * 5);
-	if(cell == NULL){printf("[Err] malloc() @ main .\n");exit(1);}
-	for(i=0;i<5;i++){
-		init_List_zero(&cell[i]);
-		sprintf(cell[i].Head,"%s","FLOAT");
-		cell[i].function = NULL;
-		cell[i].Val = v[i];
-		cell[i].function = &Function_Print_Head;
-	}
-	for(i=0;i<3;i++){
-		Function_Add_NextRtd(&precell,&cell[i]);
-	}
-	Function_Add_NextRtd(&cell[2],&cell[3]);
-	Function_Add_ArgRtd(&cell[3],&cell[4]);
-	ExFunction_Recursive_List(&precell,Function_Print_Index);
-	printf("=====\n");
 
 	//lastcell = FunctionExternal_Recursive_Liposome(&precell, NULL, (struct Liposome *(*)())precell.v_function);
 	return(0);
