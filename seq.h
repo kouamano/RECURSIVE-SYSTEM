a73486bba428df83f4e9372d343bcc5ccf118276
kouamano <amano.au1@gmail.com>
2018-12-05 15:46:24 +0900

    add

--- seq.h
+++ seq.h
@@ -5,13 +5,31 @@
 #define BUFF_LEN 1024
 #endif
 
-struct Seq {
+struct Seq {		//<conj><open><Head><close>
 	int LVself;
 	int ACself;
-	int conj;
-	int open;
+	int conj;	//is conjugate (,) => Next
+	int open;	//open list => Next
 	char *Head;
-	int close;
+	int close;	//close list => current
 	struct Seq *Next;
 	struct Seq *Parent;
 };
+
+struct Seq Function_Create_Seq(void){
+	struct Seq *seq;
+	if((seq = malloc(sizeof(struct Seq) * 1)) == NULL){
+		fprintf(stderr,"[Fail] malloc.\n");
+		exit(1);
+	}
+	(*seq).LVself = 0;
+	(*seq).ACself = 0;
+	(*seq).open = 0;
+	if(((*seq).Head = malloc(sizeof(char) * BUFF_LEN)) == NULL){
+		fprintf(stderr,"[Fail] malloc.\n");
+		exit(1);
+	}
+	(*seq).close = 0;
+	(*seq).Next = NULL;
+	(*seq).Parent = NULL;
+}
