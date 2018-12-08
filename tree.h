42601b6efb463cdb1563dfd1473fab838c50c2ce
kouamano <amano.au1@gmail.com>
2018-12-08 21:17:52 +0900

    rev

--- tree.h
+++ tree.h
@@ -15,3 +15,15 @@ struct Tree {
 	int NextCount;
 	struct Tree **Next;
 };
+
+struct Tree *Create_Tree(int H_size){
+	struct Tree *tree;
+	if((tree = malloc(sizeof(struct Tree) * 1)) == NULL){
+		fprintf(stderr,"[Fail] malloc.\n");
+		exit(1);
+	}
+	(*tree).LVself=0;
+	(*tree).ACself=0;
+	(*tree).Conj=0;
+	(*tree).Bopen=0;
+}
