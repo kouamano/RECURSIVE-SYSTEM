54c416ad2c7bd22d3eee2f189a121eca106c79c1
kouamano <amano.au1@gmail.com>
2018-12-25 09:41:39 +0900

    rev

--- DictCategory.h
+++ DictCategory.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef BUFF
-#define BUFF 1024
+#ifndef BUFF_LEN
+#define BUFF_LEN 1024
 #endif
 #include "Tree.h"
 
@@ -17,13 +17,13 @@ struct Triple {
 
 struct Triple *init_Triple_zero(struct Triple *obj){
 	(*obj).ID=0;
-	(*obj).Arrow = malloc(sizeof(struct List) * 1);
+	(*obj).Arrow = malloc(sizeof(struct Tree) * 1);
 	if((*obj).Arrow == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 	(*obj).Dcount=0;
-	(*obj).Dom = malloc(sizeof(struct List *) * 1);
+	(*obj).Dom = malloc(sizeof(struct Tree *) * 1);
 	if((*obj).Dom == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 	(*obj).Ccount=0;
-	(*obj).Cod = malloc(sizeof(struct List *) * 1);
+	(*obj).Cod = malloc(sizeof(struct Tree *) * 1);
 	if((*obj).Cod == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 }
 
@@ -31,13 +31,13 @@ struct Triple *create_Triple_zero(void){
 	struct Triple *obj;
 	obj = malloc(sizeof(struct Triple) * 1);
 	if(obj == NULL){ printf("[Fail] malloc.\n"); exit(1); }
-	(*obj).Arrow = malloc(sizeof(struct List) * 1);
+	(*obj).Arrow = malloc(sizeof(struct Tree) * 1);
 	if((*obj).Arrow == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 	(*obj).Dcount=0;
-	(*obj).Dom = malloc(sizeof(struct List *) * 1);
+	(*obj).Dom = malloc(sizeof(struct Tree *) * 1);
 	if((*obj).Dom == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 	(*obj).Ccount=0;
-	(*obj).Cod = malloc(sizeof(struct List *) * 1);
+	(*obj).Cod = malloc(sizeof(struct Tree *) * 1);
 	if((*obj).Cod == NULL){ printf("[Fail] alloc.\n"); exit(1); }
 	return(obj);
 }
