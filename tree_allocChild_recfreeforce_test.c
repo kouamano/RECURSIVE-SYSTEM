e0a912e98e6b0694d8662e26d7fcd16be80b86fb
kouamano <amano.au1@gmail.com>
2018-12-26 09:06:58 +0900

    suceeded example of free

---
+++ tree_allocChild_recfreeforce_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Tree.h"
+#ifndef BUFF_LEN
+#define BUFF_LEN 1024
+#endif
+
+void main(void){
+	//success
+	int i;
+	struct Tree *top;
+	struct Tree *child;
+	for(i=0;i<1000000;i++){
+		top = Create_Node(BUFF_LEN);
+		child = Create_Node(BUFF_LEN);
+		Add_Next(top,child);
+		Function_RecursiveFreeForce(top);
+		free(top);
+		printf("%d\n",i);
+	}
+}
