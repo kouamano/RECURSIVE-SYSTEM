b3b7119af312a66d4d7e501d310564379f9b42cd
kouamano <amano.au1@gmail.com>
2019-01-28 09:59:26 +0900

    remove extra code

--- tq.c
+++ tq.c
@@ -65,15 +65,6 @@ void init_options(struct options *opt){
 	(*opt).buff = BUFF_LEN;
 	(*opt).in[0] = '\0';
 	(*opt).war = 0;
-	//(*opt).hF = 0;
-	/*
-	(*opt).f_counter = 0;
-	(*opt).f_print_T = 0;
-	(*opt).f_print_S = 0;
-	(*opt).f_print_status = 0;
-	(*opt).f_print_hierarchy = 0;
-	(*opt).f_print_hierarchy_status = 0;
-	*/
 }
 void init_function_options(struct function_options *fopt){
 	(*fopt).f_counter = 0;
@@ -86,7 +77,6 @@ void init_function_options(struct function_options *fopt){
 
 void get_options(int optc, char **optv, struct options *opt){
 	int i = 0;
-	//(*opt).f_counter = 0;
 	for(i=0;i<optc;i++){
 		if(strcmp(optv[i],"-h") == 0){
 			(*opt).help = 1;
@@ -102,23 +92,6 @@ void get_options(int optc, char **optv, struct options *opt){
 			(*opt).hF = 1;
 		}else if(strncmp(optv[i],"in=",3) == 0){
 			sscanf(optv[i],"in=%s",(*opt).in);
-		/*
-		}else if(strncmp(optv[i],"-FT",3) == 0){
-			(*opt).f_print_T = 1;
-			(*opt).f_counter++;
-		}else if(strncmp(optv[i],"-Fst",4) == 0){
-			(*opt).f_print_status = 1;
-			(*opt).f_counter++;
-		}else if(strncmp(optv[i],"-FS",3) == 0){
-			(*opt).f_print_S = 1;
-			(*opt).f_counter++;
-		}else if(strncmp(optv[i],"-Fhst",4) == 0){
-			(*opt).f_print_hierarchy_status = 1;
-			(*opt).f_counter++;
-		}else if(strncmp(optv[i],"-Fh",3) == 0){
-			(*opt).f_print_hierarchy = 1;
-			(*opt).f_counter++;
-		*/
 		}
 	}
 }
@@ -150,12 +123,6 @@ void check_options(struct options *opt){
 	printf(" opt.buff:%d:\n",(*opt).buff);
 	printf(" opt.in:%s:\n",(*opt).in);
 	printf(" opt.war:%d:\n",(*opt).war);
-	//printf(" opt.fcount:%d:\n",(*opt).f_counter);
-	//printf(" opt.FT:%d:\n",(*opt).f_print_T);
-	//printf(" opt.FS:%d:\n",(*opt).f_print_S);
-	//printf(" opt.Fst:%d:\n",(*opt).f_print_status);
-	//printf(" opt.Fh:%d:\n",(*opt).f_print_hierarchy);
-	//printf(" opt.Fhst:%d:\n",(*opt).f_print_hierarchy_status);
 }
 void check_function_options(struct function_options *fopt){
 	printf(" functions:\n");
@@ -167,14 +134,6 @@ void check_function_options(struct function_options *fopt){
 	printf("  opt.Fhst:%d:\n",(*fopt).f_print_hierarchy_status);
 }
 
-/*
-int create_func_list(int *_flags,struct options *opt){
-	int i=0;
-	_flags = malloc(sizeof(int) * (*opt).f_counter);
-}
-*/
-
-
 int main(int argc, char **argv){
 	struct options *opt;
 	struct function_options *fopt;
@@ -182,7 +141,6 @@ int main(int argc, char **argv){
 	FILE *IN;
 	int is_open = 0;
 	int c;
-	//int *flist;
 	opt = alloc_options();
 	fopt = alloc_function_options();
 	init_options(opt);
@@ -220,7 +178,7 @@ int main(int argc, char **argv){
 	c = 1;
 	struct Tree *top;
 	top = Create_Node(BUFF_LEN);
-	c = importApp_Tree(IN,top,opt,fopt); // it cause mem leak, call many valiables every char
+	c = importApp_Tree(IN,top,opt,fopt); // removed mem leak code.
 		/*
 		if(c == '\n'){
 			function();
