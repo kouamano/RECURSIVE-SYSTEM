ca636e72171b028b2ab0cc5453959d42bda9c116
kouamano <amano.au1@gmail.com>
2019-01-28 09:51:54 +0900

    union: structure definition files

--- T-structure.h
+++ T-structure.h
@@ -11,3 +11,22 @@ struct Tree {
 	struct Tree *Parent;
 };
 
+struct options {
+        int help;
+        int stat;
+        int check;
+        int buff;
+        int war;
+        char *in;
+        int hF;
+};
+
+struct function_options {
+        int f_counter;
+        int f_print_T;
+        int f_print_S;
+        int f_print_status;
+        int f_print_hierarchy;
+        int f_print_hierarchy_status;
+};
+
