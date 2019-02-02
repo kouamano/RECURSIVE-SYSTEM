56f95fd72f1a3b9ad08a96464158bfb5cdb5e887
kouamano <amano.au1@gmail.com>
2019-02-02 22:19:56 +0900

    add escape op

---
+++ C-import_export.h
@@ -0,0 +1,376 @@
+//import/export 
+////import function
+int read_Tree(FILE *_IN, struct Tree *top, int WAR){
+	int C;
+	int DLM_ACC = 1;
+	int LIST_LV = 0;
+	int ARG_COUNT = 0;
+	char *BUFF;
+	int buf_ptr = 0;
+	struct Tree *current;
+	struct Tree *next;
+	int close = 0;
+	current = top;
+	next = NULL;
+	if((BUFF = malloc(sizeof(char) * BUFF_LEN)) == NULL){
+		printf("[Fail] malloc.\n");
+		exit(1);
+	}
+	while((C = fgetc(_IN))){
+		/* check chars */
+		if(WAR > 0){
+		printf("\n:C=%c: ",C);
+		}
+		/* set conditions */
+		if(C == '['){
+			DLM_ACC--;
+		}
+		if(C == ']'){
+			DLM_ACC++;
+		}
+		if(C == ',' && DLM_ACC > 0){
+			ARG_COUNT++;
+		}
+		if(C == '('){
+			LIST_LV++;
+		}
+		if(C == ')'){
+			LIST_LV--;
+		}
+		/* function code */
+		if(C == '('){
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			(*current).Bclose = 0;
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* create next */
+			next = Create_Node(BUFF_LEN);
+			/* add next to current / current.NextCount / next.LVself, NCself */
+			Add_Next(current,next);
+			/* set properties of next */
+			(*next).Bclose = 0;
+			(*next).Bopen = 1;
+			(*next).Conj = 0;
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			//parent = current;
+			/* set node_current */
+			current = next;
+			close = 0;
+		}else if(C == ',' && DLM_ACC > 0){
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			(*current).Bclose = 0;
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* create next */
+			next = Create_Node(BUFF_LEN);
+			/* add next */
+			Add_Next((*current).Parent,next);
+			/* set properties of next */
+			(*next).Bclose = 0;
+			(*next).Bopen = 0;
+			(*next).Conj = 1;
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			// parent = parent;
+			/* set node_current */
+			current = next;
+			close = 0;
+		}else if(C == ')'){
+			//parent = (*current).Parent;
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+				(*current).Bclose = 1;
+			}else{
+				Add_Bclose_To_Next(current);
+			}
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			// parent = parent;
+			/* set node_current */
+			if((*current).Parent != NULL){
+				current = (*current).Parent;
+			}
+			close++;
+		}else if(C == '\n'){
+			/* print BUFF */
+			BUFF[buf_ptr] = '\0';
+			//printf("%s",BUFF);
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+
+			/* clear tree */
+			close = 0;
+			return(C);
+		}else if(C == EOF){
+			close = 0;
+			return(C);
+		}else if(C == ' '){
+			;	//ignore char
+		}else{
+			/* buffering */
+			BUFF[buf_ptr] = C;
+			BUFF[buf_ptr+1] = '\0';
+			buf_ptr++;
+			close = 0;
+		}
+	}
+	return(C);
+}
+////import and apply
+int importApp_Tree(FILE *_IN, struct Tree *top, struct options *_opt, struct function_options *_fopt){
+	int WAR;
+	int C;
+	int DLM_ACC = 1;
+	int LIST_LV = 0;
+	int ARG_COUNT = 0;
+	char *BUFF;
+	int buf_ptr = 0;
+	struct Tree *current;
+	struct Tree *next;
+	int close = 0;
+	int ESC = 0;
+	WAR = (*_opt).war;
+	current = top;
+	next = NULL;
+	if((BUFF = malloc(sizeof(char) * BUFF_LEN)) == NULL){
+		printf("[Fail] malloc.\n");
+		exit(1);
+	}
+	while((C = fgetc(_IN))){
+		/* check chars */
+		if(WAR > 0){
+		printf("\n:C=%c: ",C);
+		}
+		/* set conditions */
+		if(C == '['){
+			DLM_ACC--;
+		}
+		if(C == ']'){
+			DLM_ACC++;
+		}
+		if(C == ',' && DLM_ACC > 0){
+			ARG_COUNT++;
+		}
+		if(C == '('){
+			LIST_LV++;
+		}
+		if(C == ')'){
+			LIST_LV--;
+		}
+		if(C == '\\'){
+			ESC = 1;
+		}
+		/* function code */
+		if(C == '(' && ESC == 0){
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			(*current).Bclose = 0;
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* create next */
+			next = Create_Node(BUFF_LEN);
+			/* add next to current / current.NextCount / next.LVself, NCself */
+			Add_Next(current,next);
+			/* set properties of next */
+			(*next).Bclose = 0;
+			(*next).Bopen = 1;
+			(*next).Conj = 0;
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			//parent = current;
+			/* set node_current */
+			current = next;
+			close = 0;
+		}else if(C == ',' && DLM_ACC > 0 && ESC == 0){
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			(*current).Bclose = 0;
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* create next */
+			next = Create_Node(BUFF_LEN);
+			/* add next */
+			Add_Next((*current).Parent,next);
+			/* set properties of next */
+			(*next).Bclose = 0;
+			(*next).Bopen = 0;
+			(*next).Conj = 1;
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			// parent = parent;
+			/* set node_current */
+			current = next;
+			close = 0;
+		}else if(C == ')' && ESC == 0){
+			/* confirm current */
+			BUFF[buf_ptr] = '\0';
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+				(*current).Bclose = 1;
+			}else{
+				Add_Bclose_To_Next(current);
+			}
+			/* check */
+			if(WAR > 0){
+			printf(":Pp=%ld:",(long int)(*current).Parent);
+			printf(":Cp=%ld:",(long int)current);
+			printf(":Cj=%d:",(*current).Conj);
+			printf(":Op=%d:",(*current).Bopen);
+			printf(":current=%s:",(*current).Head);
+			printf(":Cl=%d:",(*current).Bclose);
+			printf(":LV=%d:",(*current).LVself);
+			printf(":NCs=%d:",(*current).NCself);
+			printf(":NC=%d:",(*current).NextCount);
+			}
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			/* set node_parent */
+			// parent = parent;
+			/* set node_current */
+			if((*current).Parent != NULL){
+				current = (*current).Parent;
+			}
+			close++;
+		}else if(C == '\n'){
+			/* print BUFF */
+			BUFF[buf_ptr] = '\0';
+			//printf("%s",BUFF);
+			if(close == 0){
+				strcpy((*current).Head,BUFF);
+			}
+			/* clear BUFF */
+			BUFF[0] = '\0';
+			buf_ptr = 0;
+			close = 0;
+
+			/* apply functions */
+			if((*_fopt).f_print_T == 1){
+			ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_Head);
+			printf("\n");
+			}
+			if((*_fopt).f_print_S == 1){
+			ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_Head_S);
+			printf("\n");
+			}
+			if((*_fopt).f_print_status == 1){
+			ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_Status);
+			//printf("\n");
+			}
+			if((*_fopt).f_print_hierarchy == 1){
+			ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchy);
+			//printf("\n");
+			}
+			if((*_fopt).f_print_hierarchy_status == 1){
+			ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchyStatus);
+			//printf("\n");
+			}
+
+			/* clear tree */
+			Function_RecursiveFreeForce(top);
+			free(top);
+			top = Create_Node(BUFF_LEN);
+			ESC = 0;
+		}else if(C == EOF){
+			close = 0;
+			ESC = 0;
+			return(C);
+		}else{
+			/* buffering */
+			BUFF[buf_ptr] = C;
+			BUFF[buf_ptr+1] = '\0';
+			buf_ptr++;
+			close = 0;
+			if(C == '\\'){
+				ESC = 1;
+			}else{
+				ESC = 0;
+			}
+		}
+	}
+	return(C);
+}
+
---
+++ cq.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 1024
+#define BUFF_LEN 1024
+#ifndef max
+#define max(a,b) (((a)>(b))?(a):(b))
+#endif
+#ifndef min
+#define min(a,b) (((a)<(b))?(a):(b))
+#endif
+#include "../RECURSIVE-SYSTEM/T-structure.h"
+#include "../RECURSIVE-SYSTEM/T-basic_functions.h"
+#include "../RECURSIVE-SYSTEM/C-import_export.h"
+
+void status(void){
+	printf("STATUS:\n");
+	printf(" Constructing functions.\n");
+}
+
+void help(void){
+	printf("USAGE:\n");
+	printf(" tq [-h] [-s] [-c] buff=<size(int)> in=<input file> w=<print warnning> -F<x>.\n");
+	printf("  -h : help.\n");
+	printf("  -hF : function help.\n");
+	printf("  -s : stat.\n");
+	printf("  -c : check args.\n");
+	printf("  buff : set integer.\n");
+	printf("  in : set *chars (len < 1024).\n");
+	printf("  war : set integer.\n");
+}
+void function_help(void){
+	printf("  -F<x> : function symbol, e.g. \"-FS\" prints S-form.\n");
+	printf("   -FT : prints T-form.\n");
+	printf("   -FS : prints S-form.\n");
+	printf("   -Fh : prints hierarchical-form.\n");
+	printf("   -Fst : prints import status.\n");
+	printf("   -Fhst : prints import status with hierarchical-form.\n");
+}
+
+struct options *alloc_options(void){
+	struct options *p;
+	if((p = malloc(sizeof(struct options) * 1)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).in = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	return(p);
+}
+struct function_options *alloc_function_options(void){
+	struct function_options *p;
+	if((p = malloc(sizeof(struct function_options) * 1)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	return(p);
+}
+
+void init_options(struct options *opt){
+	(*opt).help = 0;
+	(*opt).stat = 0;
+	(*opt).check = 0;
+	(*opt).buff = BUFF_LEN;
+	(*opt).war = 0;
+	(*opt).in[0] = '\0';
+	(*opt).hF = 0;
+}
+void init_function_options(struct function_options *fopt){
+	(*fopt).f_counter = 0;
+	(*fopt).f_print_T = 0;
+	(*fopt).f_print_S = 0;
+	(*fopt).f_print_status = 0;
+	(*fopt).f_print_hierarchy = 0;
+	(*fopt).f_print_hierarchy_status = 0;
+}
+
+void get_options(int optc, char **optv, struct options *opt){
+	/*
+	char *opstr;
+	if((opstr = calloc(LEN,sizeof(char))) == NULL){
+		perror("[Fail] malloc()@get_options.\n");
+		exit(1);
+	}
+	*/
+	int i = 0;
+	for(i=0;i<optc;i++){
+		if(strcmp(optv[i],"-h") == 0 && strlen(optv[i]) == 2){
+			(*opt).help = 1;
+		}else if(strcmp(optv[i],"-s") == 0){
+			(*opt).stat = 1;
+		}else if(strcmp(optv[i],"-c") == 0){
+			(*opt).check = 1;
+		}else if(strncmp(optv[i],"buff=",5) == 0){
+			sscanf(optv[i],"buff=%d",&(*opt).buff);
+		}else if(strncmp(optv[i],"w=",2) == 0){
+			sscanf(optv[i],"w=%d",&(*opt).war);
+		}else if(strncmp(optv[i],"-hF",3) == 0){
+			(*opt).hF = 1;
+		}else if(strncmp(optv[i],"in=",3) == 0){
+			sscanf(optv[i],"in=%s",(*opt).in);
+		}
+	}
+}
+void get_function_options(int optc, char **optv, struct function_options *fopt){
+	int i = 0;
+	(*fopt).f_counter = 0;
+	for(i=0;i<optc;i++){
+		if(strncmp(optv[i],"-FT",3) == 0){
+			(*fopt).f_print_T = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-Fst",4) == 0){
+			(*fopt).f_print_status = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-FS",3) == 0){
+			(*fopt).f_print_S = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-Fhst",4) == 0 && strlen(optv[i]) == 5){
+			(*fopt).f_print_hierarchy_status = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-Fh",3) == 0 && strlen(optv[i]) == 3){
+			(*fopt).f_print_hierarchy = 1;
+			(*fopt).f_counter++;
+		}
+	}
+}
+
+void check_options(struct options *opt){
+	printf("OPTIONS:\n");
+	printf(" opt.buff:%d:\n",(*opt).buff);
+	printf(" opt.in:%s:\n",(*opt).in);
+	printf(" opt.war:%d:\n",(*opt).war);
+	printf(" opt.hF:%d:\n",(*opt).hF);
+}
+void check_function_options(struct function_options *fopt){
+	printf(" functions:\n");
+	printf("  opt.fcount:%d:\n",(*fopt).f_counter);
+	printf("  opt.FT:%d:\n",(*fopt).f_print_T);
+	printf("  opt.FS:%d:\n",(*fopt).f_print_S);
+	printf("  opt.Fst:%d:\n",(*fopt).f_print_status);
+	printf("  opt.Fh:%d:\n",(*fopt).f_print_hierarchy);
+	printf("  opt.Fhst:%d:\n",(*fopt).f_print_hierarchy_status);
+}
+
+int main(int argc, char **argv){
+	struct options *opt;
+	struct function_options *fopt;
+	int ie = 0;
+	FILE *IN;
+	int is_open = 0;
+	int c;
+	opt = alloc_options();
+	fopt = alloc_function_options();
+	init_options(opt);
+	init_function_options(fopt);
+	get_options(argc-1, argv+1, opt);
+	get_function_options(argc-1, argv+1, fopt);
+	if(argc == 1){
+		(*opt).help = 1;
+	}
+	if((*opt).help == 1){
+		help();
+		//function_help();
+		ie = 1;
+	}
+	if((*opt).hF == 1){
+		help();
+		function_help();
+		ie = 1;
+	}
+	if((*opt).stat == 1){
+		status();
+		ie = 1;
+	}
+	if((*opt).check == 1){
+		check_options(opt);
+		check_function_options(fopt);
+		ie = 1;
+	}
+	if(ie == 1){
+		exit(0);
+	}
+	// open file
+	if((IN = fopen((*opt).in,"r")) == NULL){
+		perror((*opt).in);
+		exit(1);
+	}
+	is_open = 1;
+
+	// main function
+	c = 1;
+	struct Tree *top;
+	top = Create_Node(BUFF_LEN);
+	c = importApp_Tree(IN,top,opt,fopt); // removed mem leak code.
+
+	// close file
+	if(is_open > 0){
+		fclose(IN);
+	}
+
+	// finish
+	return(c);
+}
