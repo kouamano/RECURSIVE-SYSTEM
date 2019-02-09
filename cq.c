97cb358cf64a7ecd46fdc81198d63b8b512a58ce
kouamano <amano.au1@gmail.com>
2019-02-09 13:15:29 +0900

    cp tq to cq

--- cq.c
+++ cq.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define LEN 1024
-#define BUFF_LEN 1024
-#ifndef max
-#define max(a,b) (((a)>(b))?(a):(b))
-#endif
-#ifndef min
-#define min(a,b) (((a)<(b))?(a):(b))
-#endif
+#include "../RECURSIVE-SYSTEM/T-definition.h"
 #include "../RECURSIVE-SYSTEM/T-structure.h"
 #include "../RECURSIVE-SYSTEM/T-basic_functions.h"
 #include "../RECURSIVE-SYSTEM/T-import_export.h"
 
+const char cdate[] = __DATE__;
+const char ctime[] = __TIME__;
+
 void status(void){
 	printf("STATUS:\n");
-	printf(" Constructing functions.\n");
+	printf(" Testing of JSON print.\n");
+	printf(" The option FMa : under construction.\n");
+	printf(" The option form : under construction.\n");
+	printf("COMPILED:\n");
+	printf(" %s\n",ctime);
+	printf(" %s\n",cdate);
 }
 
 void help(void){
 	printf("USAGE:\n");
-	printf(" tq [-h] [-s] [-c] buff=<size(int)> in=<input file> w=<print warnning> -F<x>.\n");
+	printf(" tq [-h|-hF] [-s] [-c] buff=<size(int)> in=<input file> form=<input form> w=<print warnning> -F<x>.\n");
 	printf("  -h : help.\n");
 	printf("  -hF : function help.\n");
-	printf("  -s : stat.\n");
+	printf("  -s : prints status.\n");
 	printf("  -c : check args.\n");
-	printf("  buff : set integer.\n");
-	printf("  in : set *chars (len < 1024).\n");
-	printf("  war : set integer.\n");
+	printf("  buff : set integer for buffer size to read the nodes.\n");
+	printf("  in : set input file name (length < 1024).\n");
+	printf("  form : decrear input-form\n");
+	printf("   0 single: operate whole as single line (under construction),\n");
+	printf("   1 multi: apply reference lines (under construction),\n");
+	printf("   2 individual: operate line by line.\n");
+	printf("  war : set integer for warnnig level.\n");
 }
 void function_help(void){
 	printf("  -F<x> : function symbol, e.g. \"-FS\" prints S-form.\n");
 	printf("   -FT : prints T-form.\n");
 	printf("   -FS : prints S-form.\n");
+	printf("   -FJ : prints JSON.\n");
+	printf("   -FW : prints Wolfram language.\n");
+	printf("   -FMa : prints Adjacency matrix (under construction).\n");
 	printf("   -Fh : prints hierarchical-form.\n");
 	printf("   -Fst : prints import status.\n");
 	printf("   -Fhst : prints import status with hierarchical-form.\n");
@@ -66,25 +74,22 @@ void init_options(struct options *opt){
 	(*opt).buff = BUFF_LEN;
 	(*opt).war = 0;
 	(*opt).in[0] = '\0';
+	(*opt).form = 2;
 	(*opt).hF = 0;
 }
 void init_function_options(struct function_options *fopt){
 	(*fopt).f_counter = 0;
 	(*fopt).f_print_T = 0;
 	(*fopt).f_print_S = 0;
+	(*fopt).f_print_J = 0;
+	(*fopt).f_print_W = 0;
+	(*fopt).f_print_Ma = 0;
 	(*fopt).f_print_status = 0;
 	(*fopt).f_print_hierarchy = 0;
 	(*fopt).f_print_hierarchy_status = 0;
 }
 
 void get_options(int optc, char **optv, struct options *opt){
-	/*
-	char *opstr;
-	if((opstr = calloc(LEN,sizeof(char))) == NULL){
-		perror("[Fail] malloc()@get_options.\n");
-		exit(1);
-	}
-	*/
 	int i = 0;
 	for(i=0;i<optc;i++){
 		if(strcmp(optv[i],"-h") == 0 && strlen(optv[i]) == 2){
@@ -101,6 +106,13 @@ void get_options(int optc, char **optv, struct options *opt){
 			(*opt).hF = 1;
 		}else if(strncmp(optv[i],"in=",3) == 0){
 			sscanf(optv[i],"in=%s",(*opt).in);
+		}else if(strncmp(optv[i],"form=single",11) == 0){
+			(*opt).form = 0;
+		}else if(strncmp(optv[i],"form=multi",10) == 0){
+			(*opt).form = 1;
+		}else if(strncmp(optv[i],"form=individual",15) == 0){
+			(*opt).form = 2;
+
 		}
 	}
 }
@@ -116,6 +128,15 @@ void get_function_options(int optc, char **optv, struct function_options *fopt){
 			(*fopt).f_counter++;
 		}else if(strncmp(optv[i],"-FS",3) == 0){
 			(*fopt).f_print_S = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-FJ",3) == 0){
+			(*fopt).f_print_J = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-FW",3) == 0){
+			(*fopt).f_print_W = 1;
+			(*fopt).f_counter++;
+		}else if(strncmp(optv[i],"-FMa",4) == 0){
+			(*fopt).f_print_Ma = 1;
 			(*fopt).f_counter++;
 		}else if(strncmp(optv[i],"-Fhst",4) == 0 && strlen(optv[i]) == 5){
 			(*fopt).f_print_hierarchy_status = 1;
@@ -133,12 +154,16 @@ void check_options(struct options *opt){
 	printf(" opt.in:%s:\n",(*opt).in);
 	printf(" opt.war:%d:\n",(*opt).war);
 	printf(" opt.hF:%d:\n",(*opt).hF);
+	printf(" opt.form:%d:\n",(*opt).form);
 }
 void check_function_options(struct function_options *fopt){
 	printf(" functions:\n");
 	printf("  opt.fcount:%d:\n",(*fopt).f_counter);
 	printf("  opt.FT:%d:\n",(*fopt).f_print_T);
 	printf("  opt.FS:%d:\n",(*fopt).f_print_S);
+	printf("  opt.FJ:%d:\n",(*fopt).f_print_J);
+	printf("  opt.FW:%d:\n",(*fopt).f_print_W);
+	printf("  opt.FMa:%d:\n",(*fopt).f_print_Ma);
 	printf("  opt.Fst:%d:\n",(*fopt).f_print_status);
 	printf("  opt.Fh:%d:\n",(*fopt).f_print_hierarchy);
 	printf("  opt.Fhst:%d:\n",(*fopt).f_print_hierarchy_status);
@@ -194,6 +219,7 @@ int main(int argc, char **argv){
 	struct Tree *top;
 	top = Create_Node(BUFF_LEN);
 	c = importApp_Tree(IN,top,opt,fopt); // removed mem leak code.
+	//c = importApp_Tree_OLD(IN,top,opt,fopt); // removed mem leak code.
 
 	// close file
 	if(is_open > 0){
