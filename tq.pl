dbeba650ad5b03ff129f3f036e3e22346957a1d8
kouamano <amano.au1@gmail.com>
2019-02-19 20:27:11 +0900

    rev

--- tq.pl
+++ tq.pl
@@ -1,11 +1,32 @@
 #!/bin/env perl
 $head = "";
-$rls = ();
-$script = ();
+%rls = ();
+@script = ();
+@vals = ();
 while(<>){
-	if($_ =~ /\$RE$/){
-		print $_;
-	}elsif($_ =~ /\$E$/){
-		;
+	chomp;
+	if($_ =~ /\$RE\$/){
+		$head = $_;
+	}elsif($_ =~ /\$E\$/){
+		$head = $_;
+	}elsif($_ =~ /\t->\t/){
+		($k,$v) = split(/\t->\t/,$_);
+		$rls{$k} = $v;
+	}else{
+		push(@script,$_);
 	}
+}
+$head =~ s/[^(]+//;
+$head =~ s/^\(//;
+$head =~ s/\)$//;
+@vals = split(/,/,$head);
+foreach(@script){
+	$line = $_;
+	foreach(@vals){
+		$pt = $_;
+		$pt =~ s/\$/\\\$/g;
+		$tgt = $rls{$_};
+		$line =~ s/$pt/$tgt/g;
+	}
+	print "$line\n";
 }
