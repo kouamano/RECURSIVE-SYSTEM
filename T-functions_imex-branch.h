298c3296282ea84b8673f52a4a10ac81147ae94d
kouamano <amano.au1@gmail.com>
2019-03-15 16:23:49 +0900

    reform

--- T-functions_imex-branch.h
+++ T-functions_imex-branch.h
@@ -9,6 +9,28 @@
 			}
 
 
+			/* apply status print */
+                        if((*_fopt).f_print_status == 1){
+				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_Status,_fopt,_copt);
+				printf("\n");
+                        }
+                        if((*_fopt).f_print_hierarchy == 1){
+				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchy,_fopt,_copt);
+				printf("\n");
+                        }
+                        if((*_fopt).f_print_hierarchy_status == 1){
+				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchyStatus,_fopt,_copt);
+				printf("\n");
+                        }
+
+
+			/* apply conjugation-table print */
+                        if((*_fopt).f_print_Ma == 1){
+				ExFunction_Recursive_Ser(top,(struct Tree *(*)())Function_Print_Adj,_fopt,_copt,SN);
+				printf("\n");
+                        }
+
+
 			/* apply formated print */
 			if((*_fopt).f_print_N == 1){
 				ExFunction_Recursive_Ser_MultiPrint(top, (struct Tree *(*)())Function_Print_Conj_T, (struct Tree *(*)())Function_Print_Head_SN, (struct Tree *(*)())Function_Print_Bopen_T,  (struct Tree *(*)())Function_Print_Bclose,  _fopt, _copt, SN);
@@ -63,24 +85,4 @@
 			}
 
 
-			/* apply status print */
-                        if((*_fopt).f_print_status == 1){
-				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_Status,_fopt,_copt);
-				printf("\n");
-                        }
-                        if((*_fopt).f_print_hierarchy == 1){
-				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchy,_fopt,_copt);
-				printf("\n");
-                        }
-                        if((*_fopt).f_print_hierarchy_status == 1){
-				ExFunction_Recursive(top,(struct Tree *(*)())Function_Print_HeadHierarchyStatus,_fopt,_copt);
-				printf("\n");
-                        }
-
-
-			/* apply conjugation-table print */
-                        if((*_fopt).f_print_Ma == 1){
-				ExFunction_Recursive_Ser(top,(struct Tree *(*)())Function_Print_Adj,_fopt,_copt,SN);
-				printf("\n");
-                        }
 
