7efff847eab55cfb7c924d80cb7c3c3f79bd839b
kouamano <amano.au1@gmail.com>
2019-03-20 16:55:49 +0900

    rev

--- T-functions.h
+++ T-functions.h
@@ -93,8 +93,8 @@ int is_reteral(char *string){
 		fails++;
 	}else if(strncmp(string,"$M",2) == 0){
 		fails++;
-	}else if(strncmp(string,"$R",2) == 0){
-		fails++;
+	//}else if(strncmp(string,"$R",2) == 0){
+	//	fails++;
 	}else if(strncmp(string,"$E",2) == 0){
 		fails++;
 	}else if(strncmp(string,"$X",2) == 0){
