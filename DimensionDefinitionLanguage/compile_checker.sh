7fd82b8d65b824c4a7b39c1096ce4ff2e066a0bf
kouamano <amano.au1@gmail.com>
2019-04-10 17:38:04 +0900

    new

---
+++ DimensionDefinitionLanguage/compile_checker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# checker
+##t
+echo t
+lex t.l
+yacc -dv t.y
+gcc y.tab.c lex.yy.c
+mv a.out t
+mv y.output t.output
+
+##etf
+echo et
+lex t.l
+yacc -dv et.y
+gcc y.tab.c lex.yy.c
+mv a.out et
+mv y.output et.output
+
+##eet
+echo eet
+lex t.l
+yacc -dv eet.y
+gcc y.tab.c lex.yy.c
+mv a.out eet
+mv y.output eet.output
+
+##s
+echo s
+lex s.l
+yacc -dv s.y
+gcc y.tab.c lex.yy.c
+mv a.out s
+mv y.output s.output
+
+##es
+echo es
+lex s.l
+yacc -dv es.y
+gcc y.tab.c lex.yy.c
+mv a.out es
+mv y.output es.output
+
+##ees
+echo ees
+lex s.l
+yacc -dv ees.y
+gcc y.tab.c lex.yy.c
+mv a.out ees
+mv y.output ees.output
+
+
