c6857fc7a74cf82989e3a14e001447dae13db91f
kouamano <amano.au1@gmail.com>
2019-05-07 11:58:37 +0900

    rev

--- T-functions_Executor.h
+++ T-functions_Executor.h
@@ -1,5 +1,5 @@
 /* include into T-functions_imex.h */
-void Executor(struct Tree *top, struct Tree *null_node, int C, int SN, struct options *_opt, struct function_options *_fopt, struct compile_options *_copt, struct search_options *_sopt, FILE *DATA, int EXEC_FLAG){
+void Executor(struct Tree *top, struct Tree *refTree, struct Tree *null_node, int C, int SN, struct options *_opt, struct function_options *_fopt, struct compile_options *_copt, struct search_options *_sopt, FILE *DATA, int EXEC_FLAG){
 		/* pre */
 		//* bind data (for input-form) */
 		//printf(":EXEC=%d:",EXEC_FLAG);
