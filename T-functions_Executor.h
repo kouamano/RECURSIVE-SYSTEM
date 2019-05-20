46597ae27920e243a992c28fbd0e21bc2b028acf
kouamano <amano.au1@gmail.com>
2019-05-20 18:17:12 +0900

    refine

--- T-functions_Executor.h
+++ T-functions_Executor.h
@@ -83,18 +83,18 @@ struct Tree *Executor(struct Tree *top, struct Tree *refTree, struct Tree *null_
 			}
 
 			if((*_fopt).f_print_C == 1){
-				printf("====multiline====>\n");
+				printf("\n====multiline====>\n");
 				ExFunction_Recursive_Ser_MultiPrint(hit_tree, (struct Tree *(*)())Function_Print_Conj_C, (struct Tree *(*)())Function_Print_Head, (struct Tree *(*)())Function_Print_Bopen_C, (struct Tree *(*)())Function_Print_Bclose_C, _opt,_fopt,_copt,SN);
 			}
 			///* apply status print */
                         if((*_fopt).f_print_status == 1){
-				printf("====multiline====>\n");
+				printf("\n====multiline====>\n");
 				ExFunction_Recursive(hit_tree,(struct Tree *(*)())Function_Print_Status,_opt,_fopt,_copt);
 				printf("\n");
                         }
 			///* apply conjugation-table print */
                         if((*_fopt).f_print_Ma == 1){
-				printf("====multiline====>\n");
+				printf("\n====multiline====>\n");
 				ExFunction_Recursive_Ser(hit_tree,(struct Tree *(*)())Function_Print_Adj,_opt,_fopt,_copt,SN,1);
 				printf("\n");
                         }
