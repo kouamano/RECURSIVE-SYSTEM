9a17fd5226a2e48cf2a68325c072134d67e8fded
kouamano <amano.au1@gmail.com>
2019-06-04 17:02:12 +0900

    rev

--- tq.c
+++ tq.c
@@ -534,7 +534,7 @@ int main(int argc, char **argv){
 		Executor(otop,itop,null_node,EOF,node_count,opt,_fopt,_copt,_sopt,DATA,EFLAG);
 	}
 
-	/* finish */
+	/* terminal report */
 	fprintf(stderr,"%d Nodes were operated.\n",node_count);
 
 	/* check */
@@ -542,19 +542,16 @@ int main(int argc, char **argv){
 	printf("\nIt might cause SIGSEGV, because of test for under-construction function.\n");
 	printf("\n===test===:::\n");
 	printf("6->");
-	//print_singleVal((*itop).valstr+(*itop).valPtr[6]);
 	printf(";");
 	Function_Print_nthVal(itop,6);
 	printf("\n:::===/test===\n");
 	printf("\n===test===:::\n");
 	printf("8->");
-	//print_singleVal((*itop).valstr+(*itop).valPtr[8]);
 	printf(";");
 	Function_Print_nthVal(itop,8);
 	printf("\n:::===/test===\n");
 	printf("\n===test===:::\n");
 	printf("18->");
-	//print_singleVal((*itop).valstr+(*itop).valPtr[18]);
 	printf(";");
 	Function_Recursive_Print_nthVal(itop,18);
 	printf(";");
@@ -565,7 +562,6 @@ int main(int argc, char **argv){
 	}
 	printf("\n:::===/test===\n");
 
-	/** 2 */
-
+	/* finish */
 	return(0);
 }
