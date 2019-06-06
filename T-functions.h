decfeeab348570f029221cb08220091f87e18e63
kouamano <amano.au1@gmail.com>
2019-06-06 16:01:33 +0900

    rm comment

--- T-functions.h
+++ T-functions.h
@@ -1052,7 +1052,6 @@ struct Tree *Function_Print_Head(struct Tree *tree, struct function_options *_fo
 	tmp_str = Function_Interpret_Head(tree,_copt);
 	/* TODO: if extra_stat&2 == 2 then print IProduct and return() */
 	/* print head */
-	/* TODO: add condtion for $~~ (estra_stat&1 == 1) */
 	if(((*tree).extra_stat&1) == 1){
 		printf("%s",(*tree).Head);	//normal
 	}else if((*_copt).c_counter > 0){
