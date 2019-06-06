66861a3bb3fa4a4cd98e922f6ee1ef7fca67abfc
kouamano <amano.au1@gmail.com>
2019-06-06 15:59:13 +0900

    rm comment

--- T-functions.h
+++ T-functions.h
@@ -577,9 +577,9 @@ char *Function_Interpret_Head(struct Tree *tree, struct compile_options *_copt){
 		if(((*tree).extra_stat&1) != 1){
 			(*tree).extra_stat = (*tree).extra_stat + 1;
 		}
-		/* TODO: cascade extra_stat */
 		int tmp_stat = 1;
-		ExFunction_Recursive_Set_Obj(tree, (struct Tree *(*)())Set_status, (int *)&tmp_stat);	//testing
+		/* cascading */
+		ExFunction_Recursive_Set_Obj(tree, (struct Tree *(*)())Set_status, (int *)&tmp_stat);
 		compiled++;
 	}else if(strncmp(tmp_head,"$~",2) == 0){
 		strcpy(out_head,tmp_head+2);
