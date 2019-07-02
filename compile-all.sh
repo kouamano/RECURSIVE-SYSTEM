5c5266d068ef222afd419be04cc288260e809146
kouamano <amano.au1@gmail.com>
2019-07-02 14:30:29 +0900

    add option

--- compile-all.sh
+++ compile-all.sh
@@ -2,4 +2,4 @@ gcc cts.c -o cts
 gcc cst.c -o cst
 gcc -O2 -march=native tq.c -o tq.o
 gcc -O2 -march=native tconv.c -o tconv
-gcc -DMEM_DIMENSION_STR -DMEM_VALUE_COUNT -g -O0 c_tq.c -o c_tq
+gcc -Wall -DMEM_DIMENSION_STR -DMEM_VALUE_COUNT -g -O0 c_tq.c -o c_tq
