bb9baea3bc7708d238c457968ee7073a3700342c
kouamano <amano.au1@gmail.com>
2019-07-12 10:08:15 +0900

    add

--- algebra+graph/compile.sh
+++ algebra+graph/compile.sh
@@ -1,2 +1,1 @@
-cd /home/kamano/gitsrc/utility/algebra+graph
-for fl in `ls *.c`; do echo $fl; gcc $fl -I../../include -lm  -o bin/${fl%.c}; done
+for fl in `ls *.c`; do echo $fl; gcc $fl -I../../include -lm  -o ./bin/${fl%.c}; done
