417f84b7927731f6625ac7ef5fc9a93ebf01ed16
kouamano <amano.au1@gmail.com>
2019-08-20 14:03:13 +0900

    rev

--- wxvp.c
+++ wxvp.c
@@ -6,6 +6,8 @@
 int main(int argc, char **argv){
 	printf("prog:%s:\n",argv[1]);
 	//execv(argv[1],argv+2);
-	execvp(argv[1],argv+1);
+	if(argv[1] != NULL){
+		execvp(argv[1],argv+1);
+	}
 	return(0);
 }
