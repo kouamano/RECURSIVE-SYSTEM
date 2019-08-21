66f83a2c2034700cb279d08f5cba0dd4915e8fe3
kouamano <amano.au1@gmail.com>
2019-08-21 10:18:34 +0900

    add

--- wxvp.c
+++ wxvp.c
@@ -3,11 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 
+void help(void){
+	printf("USAGE:\n");
+	printf("  wxvp <program> <arg1> <arg2> ...\n");
+}
+
 int main(int argc, char **argv){
 	printf("prog:%s:\n",argv[1]);
-	//execv(argv[1],argv+2);
 	if(argv[1] != NULL){
 		execvp(argv[1],argv+1);
+	}else{
+		help();
 	}
 	return(0);
 }
