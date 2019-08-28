4637eb1aefc416ccdfcc4d2c14a256ecaddb99f5
kouamano <amano.au1@gmail.com>
2019-08-28 10:11:22 +0900

    add

--- wxvpF.c
+++ wxvpF.c
@@ -10,6 +10,24 @@ void help(void){
 	printf("  wxvpF <file>\n");
 }
 
+char **c_alloc_mat(int num, int dim){
+        char **c;
+        int i;
+        if((c = malloc((size_t)sizeof(char *)*num)) == NULL){
+                fprintf(stderr,"[E]failed : malloc() at %ld byte.\n",(long int)sizeof(char *)*num);
+                exit(1);
+        }
+        if((c[0] = malloc((size_t)sizeof(char)*num*dim)) == NULL){
+                fprintf(stderr,"[E]failed : malloc() at %ld byte.\n",(long int)sizeof(char)*num*dim);
+                exit(1);
+        }
+        for(i=1;i<num;i++){
+                c[i] = c[i-1]+dim;
+        }
+        return(c);
+}
+
+
 int main(int argc, char **argv){
 	char **commlist = NULL;
 	int C;
@@ -19,7 +37,35 @@ int main(int argc, char **argv){
 	FILE *IN;
 
 	IN = fopen(argv[1],"r");
+	while(C = fgetc(IN)){
+                if(C == '\n'){
+                        LFcount++;
+                }
+        }
+        LFcount++;
 	fclose(IN);
+	commlist = c_alloc_mat(LFcount,MSG_SIZ);
+	exit(0);
+        IN = fopen(argv[1],"r");
+        i = 0;
+        j = 0;
+        while(C = fgetc(IN)){
+                if(C == '\n'){
+                        j++;
+                        commlist[i][j] = '\0';
+                        j = 0;
+                        i++;
+                }else if(C == EOF){
+                        j++;
+                        commlist[i][j] = '\0';
+                        break;
+                }else{
+                        commlist[i][j] = C;
+                        j++;
+                }
+        }
+	fclose(IN);
+
 	/*
 	printf("prog:%s:\n",argv[1]);
 	if(argv[1] != NULL){
