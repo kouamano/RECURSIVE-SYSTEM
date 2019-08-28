9b288659053f5a6ebc3ba4842f2832ab1dcc7853
Koichi <SAKAMOTO.Koichi@nims.go.jp>
2019-08-28 14:29:45 +0900

    compile-all.sh : compile option (-D) delete

--- compile-all.sh
+++ compile-all.sh
@@ -2,5 +2,5 @@ gcc cts.c -o cts
 gcc cst.c -o cst
 gcc -O2 -march=native tq.c -o tq.o
 gcc -O2 -march=native tconv.c -o tconv
-gcc -Wall -DMEM_DIMENSION_STR -DMEM_VALUE_COUNT -g -O0 c_tq.c -o c_tq.o
-gcc -Wall -DMEM_DIMENSION_STR -DMEM_VALUE_COUNT -g -O2 c_tq.c -o cq.o
+gcc -Wall -g -O0 c_tq.c -o c_tq.o
+gcc -Wall -O2 c_tq.c -o cq.o
