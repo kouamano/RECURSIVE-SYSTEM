589881c00fe8fc465c659976fe3f6380bd1b1d5f
kouamano <amano.au1@gmail.com>
2019-09-13 15:02:08 +0900

    update

--- tq.c
+++ tq.c
@@ -19,7 +19,6 @@ void status(void){
 	printf(" - Under construction\n");
 	printf("\n");
 	printf("  * Inner product.\n");
-	printf("  * Double unpack.\n");
 	//printf("  * Riteralize.\n");
 	printf("\n");
 	printf(" - COMPILED@\n");
@@ -125,7 +124,7 @@ void list_builtins(void){
 	printf("  * $~$ : <operator>: riteral head.\n");
 	printf("  * $~~$ : <operator>: riteral tree.\n");
 	printf("  * $U$ : <operator>: unpack single level.\n");
-	printf("  * $UU$ : <operator>: unpack tree(under construction).\n");
+	printf("  * $UU$ : <operator>: unpack tree (test ver.).\n");
 	printf("  * $PI$ : <operator>: inner production for binded data.\n");
 	printf("  * $$ : <name> prefix: dictionary reference (term ID).\n");
 	printf("  * $X$ : <name> prefix: dictionary reference (term string).\n");
