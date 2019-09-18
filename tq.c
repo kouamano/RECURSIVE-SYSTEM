76f06c2b8157c8007b79f4da1e8ad8f0ef8cd9ee
kouamano <amano.au1@gmail.com>
2019-09-18 09:54:20 +0900

    update

--- tq.c
+++ tq.c
@@ -680,7 +680,7 @@ int main(int argc, char **argv){
 		is_iopen = 1;
 		/** import tree */
 		node_count = 0;
-		EFLAG = (*opt).Pin+1+8;
+		EFLAG = (*opt).Pin+1+8;	//Pin: 2
 		itop = import_Tree(IN,opt,_fopt,_copt,_sopt,&node_count,EFLAG,DATA);
 		Executor(itop,null_node,null_node,EOF,node_count,opt,_fopt,_copt,_sopt,DATA,EFLAG);	
 		/** close in-file */
@@ -702,7 +702,7 @@ int main(int argc, char **argv){
 		}
 		is_oopen = 1;
 		/** import tree */
-		EFLAG = (*opt).Pout+4;
+		EFLAG = (*opt).Pout+4;	//Pout: 2
 		otop = import_Tree(IN,opt,_fopt,_copt,_sopt,&node_count,EFLAG,NULL);
 		Executor(otop,itop,null_node,EOF,node_count,opt,_fopt,_copt,_sopt,DATA,EFLAG);
 		/** close file */
