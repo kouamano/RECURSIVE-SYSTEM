3c35413d8ca60f7514af8534ab0040c4177e716e
kouamano <amano.au1@gmail.com>
2020-01-17 10:09:23 +0900

    rev

--- CT-functions_Executor.h
+++ CT-functions_Executor.h
@@ -93,19 +93,19 @@ NODE Executor(NODE top, NODE refTree, NODE null_node, int C, int SN, struct opti
 			}
 
 			if((*_fopt).f_print_C == 1){
-				printf("\n====multiline====>\n");
+				printf("====multiline====>\n");
 				ExFunction_Recursive_Ser_MultiPrint(hit_tree, (NODE (*)())Function_Print_Conj_C, (NODE (*)())Function_Print_Head, (NODE (*)())Function_Print_Bopen_C, (NODE (*)())Function_Print_Bclose_C, _opt,_fopt,_copt,SN);
 				printf("<====multiline====\n");
 			}
 			/*** apply status print */
                         if((*_fopt).f_print_status == 1){
-				printf("\n====multiline====>\n");
+				printf("====multiline====>\n");
 				ExFunction_Recursive(hit_tree,(NODE (*)())Function_Print_Status,_opt,_fopt,_copt);
 				printf("<====multiline====\n");
                         }
 			/*** apply conjugation-table print */
                         if((*_fopt).f_print_Ma == 1){
-				printf("\n====multiline====>\n");
+				printf("====multiline====>\n");
 				ExFunction_Recursive_Ser(hit_tree,(NODE (*)())Function_Print_Adj,_opt,_fopt,_copt,SN,1);
 				printf("<====multiline====\n");
                         }
