3c1624d5db9acf2d81d77af06bfa5d051f5f5afd
kouamano <amano.au1@gmail.com>
2020-05-07 13:48:29 +0900

    rev

--- compile-all.sh
+++ compile-all.sh
@@ -1,7 +1,7 @@
 gcc cts.c -o cts
 gcc cst.c -o cst
+gcc tconv.c -o tconv
 gcc -Wall -O2 -march=native tq.c -o tq.o
-gcc -Wall -O2 -march=native tconv.c -o tconv
 gcc -Wall -g -O0 c_tq.c -o c_tq.o
 gcc -Wall -O2 c_tq.c -o cq.o
 #sudo install tq.o /usr/local/bin/tq
