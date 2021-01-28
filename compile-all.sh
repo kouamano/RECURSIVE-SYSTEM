ee9c7c5b01dca27e79a2995bdfc736dad906b37b
KOU AMANO <amano.au1@gmail.com>
2021-01-28 16:00:06 +0900

    rev

--- compile-all.sh
+++ compile-all.sh
@@ -1,7 +1,7 @@
 gcc cts.c -o cts
 gcc cst.c -o cst
 gcc tconv.c -o tconv
-gcc toPsdJSIN.c -o toPsdJSIN
+gcc toPsdJSON.c -o toPsdJSON
 gcc -Wall -O2 -march=native tq.c -o tq.o
 gcc -Wall -g -O0 c_tq.c -o c_tq.o
 gcc -Wall -O2 c_tq.c -o cq.o
