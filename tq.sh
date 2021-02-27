1109dc2f3bbf895f70bc46771824ad5a28744671
KOU AMANO <amano.au1@gmail.com>
2021-02-27 14:12:40 +0900

    add

--- tq.sh
+++ tq.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # this is a tq meta command
 # Needs: tq.o, c_tq.o
+
+if [ $# -eq 0 ]; then
+  echo "Usage:"
+  echo "  tq target=<file>"
+  echo "  tq -h"
+  exit;
+fi
+
 if [ $1 = "-h" ]; then
-  echo ==tq==
+  echo '==tq=='
   ./tq.o -h
-  echo ==c_tq==
+  echo '==c_tq=='
   ./c_tq.o -h
   exit;
 else
