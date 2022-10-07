b8cf99050e17510465806f016ad3a30bd3cc9920
kouamano <amano.au1@gmail.com>
2022-10-07 16:16:16 +0900

    update

--- T-functions.h
+++ T-functions.h
@@ -745,7 +745,7 @@ void Print_Smems(struct Tree *tree){
 	printf(":C=%d:",(*tree).NextCount);
 	printf(":stat=%d:",(*tree).builtin_flag);
 }
-void Function_Print_Status(struct Tree *tree){
+void Function_Print_Status(struct Tree *tree, struct options *o_dummy, int i_dummy){
 	Print_Smems(tree);
 	printf("\n");
 }
@@ -788,7 +788,7 @@ int Function_Print_Adj(struct Tree *tree, struct options *_opt, int nodes){
 	printf("\n");
 	return(nodes);
 }
-int Function_Print_Linkage(struct Tree *tree){
+int Function_Print_Linkage(struct Tree *tree, struct options *o_dummy, int i_dummy){
 	if((*tree).Parent != NULL){
 		printf("\"%s\" -> ",(*tree).Parent->Head);
 		printf("\"%s\"\n",(*tree).Head);
