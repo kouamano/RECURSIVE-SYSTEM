6fd6f6d29bc24febf7aecfcbff3ede1bc99caf39
kouamano <amano.au1@gmail.com>
2022-10-09 23:08:36 +0900

    temporary

---
+++ xq.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 1024
+
+struct options {
+	int help;
+	int stat;
+	int check;
+	char *file;
+	int buff;
+	int nodes;
+	int blocks;
+};
+
+void help(void){
+	printf("USAGE:\n");
+	printf(" xq [-h] [-s] [-c] if=<inputfile> buf=<buf-size> node=<nodes> nlock=<blocks>.\n");
+	printf("  -h : help.\n");
+	printf("  -s : stat.\n");
+	printf("  -c : check args.\n");
+	printf("  inputfile : input file.\n");
+	printf("  buf : buffer size.\n");
+	printf("  node : number of nodes.\n");
+	printf("  block : number of blocks.\n");
+}
+
+void status(void){
+	printf("STATUS:\n");
+	printf(" under construction.\n");
+}
+
+struct options *alloc_options(void){
+	struct options *p;
+	if((p = malloc(sizeof(struct options) * 1)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).file = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	return(p);
+}
+
+void init_options(struct options *opt){
+	(*opt).help = 0;
+	(*opt).stat = 0;
+	(*opt).check = 0;
+	(*opt).file[0] = '\0';
+	(*opt).buff = LEN;
+	(*opt).nodes = LEN;
+	(*opt).blocks = LEN;
+}
+
+void get_options(int optc, char **optv, struct options *opt){
+	int i = 0;
+	for(i=0;i<optc;i++){
+		if(strcmp(optv[i],"-h") == 0){
+			(*opt).help = 1;
+		}else if(strcmp(optv[i],"-s") == 0){
+			(*opt).stat = 1;
+		}else if(strcmp(optv[i],"-c") == 0){
+			(*opt).check = 1;
+		}else if(strncmp(optv[i],"if=",3) == 0){
+			sscanf(optv[i],"if=%s",(*opt).file);
+		}else if(strncmp(optv[i],"buf=",4) == 0){
+			sscanf(optv[i],"buf=%d",&(*opt).buff);
+		}else if(strncmp(optv[i],"nodes=",6) == 0){
+			sscanf(optv[i],"nodes=%d",&(*opt).nodes);
+		}else if(strncmp(optv[i],"blocks=",7) == 0){
+			sscanf(optv[i],"nodes=%d",&(*opt).blocks);
+		}
+	}
+}
+
+void check_options(struct options *opt){
+	printf("OPTIONS:\n");
+	printf(" opt.file:%s:\n",(*opt).file);
+}
+
+int main(int argc, char **argv){
+	struct options *opt;
+	int ie = 0;
+	opt = alloc_options();
+	init_options(opt);
+	get_options(argc-1, argv+1, opt);
+	if(argc == 1){
+		(*opt).help = 1;
+	}
+	if((*opt).help == 1){
+		help();
+		ie = 1;
+	}
+	if((*opt).stat == 1){
+		status();
+		ie = 1;
+	}
+	if((*opt).check == 1){
+		check_options(opt);
+		ie = 1;
+	}
+	if(ie == 1){
+		exit(0);
+	}
+	return(0);
+}
