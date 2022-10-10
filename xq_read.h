8d8d82b83c3a39c6166f552967a0a6a55fa56a25
kouamano <amano.au1@gmail.com>
2022-10-10 19:58:18 +0900

    add

--- xq_read.h
+++ xq_read.h
@@ -1,3 +1,7 @@
-int read_x(struct options *opt, FILE *IN, struct Block *Bl, int start_Block){
+int read_x(struct options *opt, FILE *IN, struct Block *Bl, int start_Bl, int start_Nd, int start_Lv){
+	int currentC = 0;
+	int current_Bl = start_Bl;
+	int current_Nd = start_Nd;
+	int current_Lv = start_Lv;
 	return(0);
 }
