a460196902338e7b331769267fed8177a7c65821
kouamano <amano.au1@gmail.com>
2022-10-10 21:39:08 +0900

    add

--- xq_block.h
+++ xq_block.h
@@ -15,8 +15,22 @@ struct Block {
 	int *next_Bl;
 };
 
+struct Block *alloc_Block(int size){
+	struct Block *p;
+	if((p = malloc(sizeof(struct Block) * size)) == NULL){
+		perror("alloc_Block()\n");
+		exit(1);
+	}
+	return(p);
+}
+
 int set_Block_type(struct Block *Bl, int target_Bl, int type){
-	return(0);
+	int rt = 0;
+	if(((Bl[target_Bl].Bltype)&type) != type){
+		Bl[target_Bl].Bltype += type;
+		rt = type;
+	}
+	return(rt);
 }
 
 int create_top_Nd(struct Block *Bl){
