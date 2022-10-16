570535969efd443268d1d0e376d0a9745921e908
kouamano <amano.au1@gmail.com>
2022-10-16 23:04:47 +0900

    add

--- xtq_executer.h
+++ xtq_executer.h
@@ -1,3 +1,4 @@
 void Executer(struct Block *Bl, int target_Bl, struct options opt, struct Bl_tree_report report){
+	ExPrint_seq_BlSt(Bl, target_Bl, opt, report);
 	ExPrint_seq_Bl(Bl,target_Bl,opt,report);
 }
