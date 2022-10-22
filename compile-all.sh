9b02744c341fe1f4592bc844534ba73709079ae0
kouamano <amano.au1@gmail.com>
2022-10-22 20:49:35 +0900

    update

--- compile-all.sh
+++ compile-all.sh
@@ -6,4 +6,5 @@ gcc indentToT.c -o indentToT
 gcc -Wall -O2 -march=native tq.c -o tq.o
 gcc -Wall -g -O0 c_tq.c -o c_tq.o
 gcc -Wall -O2 c_tq.c -o cq.o
+./compile-x.sh
 #sudo install tq.o /usr/local/bin/tq
