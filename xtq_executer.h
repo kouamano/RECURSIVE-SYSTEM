efb7ce18c967afc95f08089cec553b13f24d8903
kouamano <amano.au1@gmail.com>
2022-10-22 16:40:26 +0900

    add

--- xtq_executer.h
+++ xtq_executer.h
@@ -2,20 +2,22 @@ void Executer(struct Block *Bl, int target_Bl, struct options opt, struct Bl_tre
 	int output = 0;
 	if(opt.pbs == 1){
 		ExPrint_seq_BlSt(Bl, target_Bl, opt, report);
-		int output = 1;
+		output = 1;
 	}
 	if(output > 0){
 		printf("\n==== ==== ==== ====\n");
 	}
 	if(opt.pb == 1){
 		ExPrint_seq_Bl(Bl,target_Bl,opt,report);
-		int output = 1;
+		output = 1;
+	}else{
+		output = 0;
 	}
 	if(output > 0){
 		printf("\n==== ==== ==== ====\n");
 	}
 	if(opt.ptf == 1){
 		ExPrint_seq_Bl_T(Bl, target_Bl, opt, report);
-		int output = 1;
+		output = 1;
 	}
 }
