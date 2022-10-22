c0cd8acc2c6809c3cbd09875a07944af7d277406
kouamano <amano.au1@gmail.com>
2022-10-22 20:51:14 +0900

    update

---
+++ xtq.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* definition and structure */
+#include "./xtq_defstrc.h"
+
+/* help */
+void help(void){
+	printf("USAGE:\n");
+	printf(" xtq [-h] [-s] [-c] [-pt] [-pn] [-pB] [-pBS] [-pT] in=<input file> buff=<buffer size> blocks=<blocks> pf=<print format> BH=<block header> BF=<block footer> SH=<status header> SF=<status footer>.\n");
+	printf("  -h : help.\n");
+	printf("  -s : status.\n");
+	printf("  -c : check args.\n");
+	printf("  -pt : print terminal chars.\n");
+	printf("  -pn : print LF after block.\n");
+	printf("  -pB : print blocks.\n");
+	printf("  -pBS : print blocks with status.\n");
+	printf("  -pT : print blocks with T-form.\n");
+	printf("  input file : input file.\n");
+	printf("  buffer size : buffer size.\n");
+	printf("  blocks : number of blocks.\n");
+	printf("  print format : format for block (default:\"%%s\").\n");
+	printf("  block header : header for B option.\n");
+	printf("  block footer : footer for B option.\n");
+	printf("  status header : header for S option.\n");
+	printf("  status footer : footer for S option.\n");
+}
+void status(void){
+	printf("STATUS:\n");
+	printf("  complete primal function.\n");
+}
+
+/* option */
+struct options *alloc_options(void){
+	struct options *p;
+	if((p = malloc(sizeof(struct options) * 1)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).file = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).pf = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).BH = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).BF = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).SH = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).SF = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	return(p);
+}
+void init_options(struct options *opt){
+	(*opt).help = 0;
+	(*opt).stat = 0;
+	(*opt).check = 0;
+	(*opt).file[0] = '\0';
+	(*opt).buff = LEN;
+	(*opt).blocks = LEN;
+	(*opt).pt = 0;
+	(*opt).pn = 0;
+	(*opt).pb = 0;
+	(*opt).pbs = 0;
+	(*opt).ptf = 0;
+	strcpy((*opt).pf,"%s");
+	strcpy((*opt).SH,"[[[");
+	strcpy((*opt).SF,"]]]");
+	(*opt).BH[0] = '\0';
+	(*opt).BF[0] = '\0';
+}
+void get_options(int optc, char **optv, struct options *opt){
+	int i = 0;
+	for(i=0;i<optc;i++){
+		if(strcmp(optv[i],"-h") == 0){
+			(*opt).help = 1;
+		}else if(strcmp(optv[i],"-s") == 0){
+			(*opt).stat = 1;
+		}else if(strcmp(optv[i],"-c") == 0){
+			(*opt).check = 1;
+		}else if(strncmp(optv[i],"in=",3) == 0){
+			sscanf(optv[i],"in=%s",(*opt).file);
+		}else if(strncmp(optv[i],"buff=",5) == 0){
+			sscanf(optv[i],"buff=%d",&(*opt).buff);
+		}else if(strncmp(optv[i],"blocks=",7) == 0){
+			sscanf(optv[i],"blocks=%d",&(*opt).blocks);
+		}else if(strncmp(optv[i],"-pt",3) == 0){
+			(*opt).pt = 1;
+		}else if(strncmp(optv[i],"-pn",3) == 0){
+			(*opt).pn = 1;
+		}else if(strncmp(optv[i],"-pBS",4) == 0){
+			(*opt).pbs = 1;
+		}else if(strncmp(optv[i],"-pB",3) == 0){
+			(*opt).pb = 1;
+		}else if(strncmp(optv[i],"-pT",3) == 0){
+			(*opt).ptf = 1;
+		}else if(strncmp(optv[i],"pf=",3) == 0){
+			sscanf(optv[i],"pf=%s",(*opt).pf);
+
+		}else if(strncmp(optv[i],"BH=",3) == 0){
+			sscanf(optv[i],"BH=%s",(*opt).BH);
+
+		}else if(strncmp(optv[i],"BF=",3) == 0){
+			sscanf(optv[i],"BF=%s",(*opt).BF);
+
+		}else if(strncmp(optv[i],"SH=",3) == 0){
+			sscanf(optv[i],"SH=%s",(*opt).SH);
+
+		}else if(strncmp(optv[i],"SF=",3) == 0){
+			sscanf(optv[i],"SF=%s",(*opt).SF);
+		}
+	}
+}
+void check_options(struct options *opt){
+	printf("OPTIONS:\n");
+	printf(" opt.file:%s:\n",(*opt).file);
+	printf(" opt.buff:%d:\n",(*opt).buff);
+	printf(" opt.blocks:%d:\n",(*opt).blocks);
+	printf(" opt.pt:%d:\n",(*opt).pt);
+	printf(" opt.pn:%d:\n",(*opt).pn);
+	printf(" opt.pb:%d:\n",(*opt).pb);
+	printf(" opt.pbs:%d:\n",(*opt).pbs);
+	printf(" opt.ptf:%d:\n",(*opt).ptf);
+	printf(" opt.pf:%s:\n",(*opt).pf);
+	printf(" opt.BH:%s:\n",(*opt).BH);
+	printf(" opt.BF:%s:\n",(*opt).BF);
+	printf(" opt.SH:%s:\n",(*opt).SH);
+	printf(" opt.SF:%s:\n",(*opt).SF);
+}
+
+/* block operation */
+#include "./xtq_block.h"
+#include "./xtq_block_T.h"
+
+/* read operation */
+#include "./xtq_read.h"
+
+/* executer */
+#include "./xtq_executer.h"
+
+/* main */
+int main(int argc, char **argv){
+	/** vals **/
+	struct options *opt;
+	int ie = 0;
+	int if_open = 0;
+	FILE *IN = NULL;
+	struct Block *Bl = NULL;
+	/** initialize **/
+	opt = alloc_options();
+	init_options(opt);
+	get_options(argc-1, argv+1, opt);
+	/** arg processing **/
+	if(argc == 1){
+		(*opt).help = 1;
+	}
+	if((*opt).help == 1){
+		help();
+		ie = 1;
+	}
+	if((*opt).stat == 1){
+		status();
+		ie = 1;
+	}
+	if((*opt).check == 1){
+		check_options(opt);
+		ie = 1;
+	}
+	if(ie == 1){
+		exit(0);
+	}
+	/** create block **/
+	Bl = alloc_Bl_arr((*opt).blocks);
+	/*** create top node ***/
+	struct Bl_create_opt create_opt;
+	create_opt.str_size = 1;
+	create_Nd(Bl,0,create_opt);
+	/** input **/
+	if((IN = fopen((*opt).file,"r")) == NULL){
+		perror((*opt).file);
+		exit(1);
+	}
+	if_open = 1;
+	struct Bl_tree_report *report;
+	report = alloc_Bl_tree_report();
+	read_x(opt, IN, Bl, 0, 0, 1, 0, report);
+	if(if_open == 1){
+		fclose(IN);
+	}
+	/** operation **/
+	//print_tree_report(report);
+	Executer(Bl,0,*opt,*report);
+	/** finalize **/
+	return(0);
+}
---
+++ xtq_block.h
@@ -0,0 +1,174 @@
+/* functions */
+/** alloc **/
+struct Block *alloc_Bl_arr(int size){
+	struct Block *p;
+	if((p = malloc(sizeof(struct Block) * size)) == NULL){
+		perror("alloc_Block()\n");
+		exit(1);
+	}
+	return(p);
+}
+
+struct Bl_tree_report *alloc_Bl_tree_report(void){
+	struct Bl_tree_report *p;
+	if((p = malloc(sizeof(struct Bl_tree_report) * 1)) == NULL){
+		perror("alloc_Bl_tree_report");
+		exit(1);
+	}
+	return(p);
+}
+
+/** create, edit, test **/
+int create_Nd(struct Block *Bl, int No, struct Bl_create_opt create_opt){
+	//Bl[No].ser = 0;
+	Bl[No].Lv = 0;
+	Bl[No].Bltype = 0;
+	Bl[No].parent = -1;
+	if((Bl[No].str = malloc(sizeof(char) * create_opt.str_size)) == NULL){
+		perror("create_Nd");
+		exit(1);
+	}
+	Bl[No].str[0] = '\0';
+	Bl[No].nexts = 0;
+	Bl[No].next_Bl = NULL;
+	return(No);
+}
+
+int start_cdata(char *BUFF, const int counter, int in_cdata){
+	int start = 0;
+	BUFF[counter] = '\0';
+	if(strncmp(BUFF,THead_CDATA,strlen(THead_CDATA)) == 0){
+		start = 1;
+		//debug
+		//printf("{startCDATA}");
+	}
+	return(start);
+}
+
+int end_cdata(char *BUFF, const int counter, int in_cdata){
+	int end = 0;
+	BUFF[counter] = '\0';
+	if(strncmp(BUFF+strlen(BUFF)-strlen(TTail_CDATA),TTail_CDATA,strlen(TTail_CDATA)) == 0){
+		end = 1;
+		//debug
+		//printf("{endCDATA}");
+	}
+	return(end);
+}
+
+int check_Bl_type(char *BUFF, const int current){
+	int out = current;
+	//CDATA
+	if(strncmp(BUFF,THead_CDATA,strlen(THead_CDATA)) == 0){
+		if((out&BType_iTag) != BType_iTag){
+			out = BType_iTag;
+		}
+	}
+	//comment itag
+	else if((strncmp(BUFF,THead_comment,strlen(THead_comment)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_comment),TTail_comment,strlen(TTail_comment)) == 0)){
+		//debug
+		//printf("{in_coment}");
+		if((out&BType_iTag) != BType_iTag){
+			out = BType_iTag;
+		}
+	}
+	//proc itag
+	else if((strncmp(BUFF,THead_proc,strlen(THead_proc)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_proc),TTail_proc,strlen(TTail_proc)) == 0)){
+		//debug
+		//printf("{in_proc}");
+		if((out&BType_iTag) != BType_iTag){
+			out = BType_iTag;
+		}
+	}
+	//other itag
+	else if((strncmp(BUFF,THead_other,strlen(THead_other)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_other),TTail_other,strlen(TTail_other)) == 0)){
+		//debug
+		//printf("{in_ot}");
+		if((out&BType_iTag) != BType_iTag){
+			out = BType_iTag;
+		}
+	}
+	//emp itag
+	else if((strncmp(BUFF,THead_emp,strlen(THead_emp)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_emp),TTail_emp,strlen(TTail_emp)) == 0)){
+		//debug
+		//printf("{in_emp}");
+		if((out&BType_iTag) != BType_iTag){
+			out = BType_iTag;
+		}
+	}
+	//end tag
+	else if((strncmp(BUFF,THead_end,strlen(THead_end)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_end),TTail_end,strlen(TTail_end)) == 0)){
+		if((out&BType_eTag) != BType_eTag){
+			out = BType_eTag;
+		}
+	}
+	//start tag
+	else if((strncmp(BUFF,THead_start,strlen(THead_start)) == 0) && (strncmp(BUFF+strlen(BUFF)-strlen(TTail_start),TTail_start,strlen(TTail_start)) == 0)){
+		if((out&BType_sTag) != BType_sTag){
+			out = BType_sTag+BType_Node;
+		}
+	}
+	//body
+	else{
+		if((out&BType_Body) != BType_Body){
+			out = BType_Body;
+		}
+	}
+	return(out);
+}
+
+void set_Block(struct Block *Bl, int No, int type, int lv, int pn, char *buff){
+	int len = 0;
+	len = strlen(buff);
+	if((Bl[No].str = malloc(sizeof(char) * (len+1))) == NULL){
+		perror("set_Block\n");
+		exit(1);
+	}
+	strcpy(Bl[No].str,buff);
+	Bl[No].Lv = lv;
+	Bl[No].Bltype = type;
+	Bl[No].parent = pn;
+}
+
+/** print **/
+void print_tree_report(struct Bl_tree_report *report){
+	fprintf(stderr,"==== ==== ==== ==== ====\n");
+	fprintf(stderr,"followings were operated:\n");
+	fprintf(stderr,"Bls:%d\n",(*report).Bls);
+	fprintf(stderr,"Nds:%d\n",(*report).Nds);
+	fprintf(stderr,"Chrs:%d\n",(*report).Chrs);
+	fprintf(stderr,"==== ==== ==== ==== ====\n");
+}
+
+void ExPrint_seq_Bl(struct Block *Bl, int target, struct options opt, struct Bl_tree_report report){
+	int i;
+	for(i=target;i<report.Bls;i++){
+		//block
+		printf("%s",opt.BH);
+		printf(opt.pf,Bl[i].str);
+		printf("%s",opt.BF);
+		//tail
+		if(opt.pn == 1){
+			printf("\n");
+		}
+	}
+}
+
+void ExPrint_seq_BlSt(struct Block *Bl, int target, struct options opt, struct Bl_tree_report report){
+	int i;
+	for(i=target;i<report.Bls;i++){
+		//status
+		printf("%s",opt.SH);
+		printf("Bl%d,Ty%d,Lv%d,Pa%d",i,Bl[i].Bltype,Bl[i].Lv,Bl[i].parent);
+		printf("%s",opt.SF);
+		//block
+		printf("%s",opt.BH);
+		printf(opt.pf,Bl[i].str);
+		printf("%s",opt.BF);
+		//tail
+		if(opt.pn == 1){
+			printf("\n");
+		}
+	}
+}
+
