ec61fd64330956bc8f65ca70646db1d8b9241ab9
kouamano <amano.au1@gmail.com>
2022-10-23 11:00:49 +0900

    update

--- xtq.c
+++ xtq.c
@@ -201,8 +201,8 @@ int main(int argc, char **argv){
 		fclose(IN);
 	}
 	/** operation **/
-	//print_tree_report(report);
 	Executer(Bl,0,*opt,*report);
+	print_tree_report(report);
 	/** finalize **/
 	return(0);
 }
