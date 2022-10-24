ab258672f47a96298911485f8f93a45990d7e77e
kouamano <amano.au1@gmail.com>
2022-10-24 17:36:55 +0900

    rename

--- compile-all.sh
+++ compile-all.sh
@@ -2,7 +2,7 @@ gcc cts.c -o cts
 gcc cst.c -o cst
 gcc tconv.c -o tconv
 gcc toPsdJSON.c -o toPsdJSON
-gcc indentToT.c -o indentToT
+gcc iq.c -o iq
 gcc -Wall -O2 -march=native tq.c -o tq.o
 gcc -Wall -g -O0 c_tq.c -o c_tq.o
 gcc -Wall -O2 c_tq.c -o cq.o
