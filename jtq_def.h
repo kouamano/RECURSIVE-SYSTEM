0a6748585109f4c3bb62deec14df6d1d4969f5b8
kouamano <amano.au1@gmail.com>
2022-11-12 17:27:13 +0900

    add

--- jtq_def.h
+++ jtq_def.h
@@ -1,3 +1,8 @@
+/* size */
 #define LEN 1024
+/* type */
 #define Type_Dict 1
-
+#define Type_Arr 2
+#define Type_KV 4
+#define Type_Key 8
+#define Type_Val 16
--- jtq_read.h
+++ jtq_read.h
@@ -1,8 +1,18 @@
+int sw_esc(int *esc){
+	if(*esc == 0){
+		*esc = 1;
+	}else{
+		*esc = 0;
+	}
+	return(*esc);
+}
 void read_json(struct options *opt, FILE *IN){
 	int current_C = 0;
 	int current_Lv = 0;
 	int current_Nd = 0;
 	int current_NdType = -1;
+	int DQ_ESC = 0;
+	int BS_ESC = 0;
 	char *BUFF = NULL;
 	while((current_C = fgetc(IN))){
 	}
