212b83385d13f48c23c96d947e54964c55382773
kouamano <amano.au1@gmail.com>
2022-11-12 16:36:19 +0900

    add

--- jtq_read.h
+++ jtq_read.h
@@ -1,5 +1,8 @@
 void read_json(struct options *opt, FILE *IN){
 	int current_C = 0;
+	int current_Lv = 0;
+	int current_Nd = 0;
+	int current_NdType = -1;
 	char *BUFF = NULL;
 	while((current_C = fgetc(IN))){
 	}
