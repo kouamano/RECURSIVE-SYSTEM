776d924be35eaffd49b3853063b952da8aa63cad
kouamano <amano.au1@gmail.com>
2022-11-13 19:32:02 +0900

    add

--- jtq_def.h
+++ jtq_def.h
@@ -3,10 +3,12 @@
 /* type */
 /** conjugator **/
 #define CjType_C 1
-#define CjType_AA 2
-#define CjType_AD 3
-#define CjType_DD 4 
-#define CjType_DA 5 
+#define CjType_A 2	//中間ノード ']'
+#define CjType_D 3	//中間ノード '}'
+#define CjType_AA 11
+#define CjType_AD 12
+#define CjType_DD 21 
+#define CjType_DA 22 
 /** node **/
 #define NdType_Dict 1
 #define NdType_Arr 2
