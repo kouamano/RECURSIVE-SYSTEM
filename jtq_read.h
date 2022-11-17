08dfe4238f8f828cbe588446d65802a246a39de3
kouamano <amano.au1@gmail.com>
2022-11-17 20:19:08 +0900

    rev

--- jtq_read.h
+++ jtq_read.h
@@ -44,16 +44,10 @@ void read_json(struct options *opt, FILE *IN, struct Nd *NdArr){
 			//Lv
 			current_Lv++;
 			//BUFF to Head & //Node set
-			if(prev_C != ']' && prev_C != '}'){	//中間ノード
+			//通常ノード
 			BUFF[BUFF_counter] = '\0';
 			BUFF_counter = 0;
 			printf("\n<<<%s>>>\n",BUFF);
-			}
-			else {					//通常ノード
-			BUFF[BUFF_counter] = '\0';
-			BUFF_counter = 0;
-			printf("\n<<<%s>>>\n",BUFF);
-			}
 
 			//Parent operation
 			if(prev_C != ']' && prev_C != '}'){
