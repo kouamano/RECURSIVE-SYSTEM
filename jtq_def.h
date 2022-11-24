1f33ca83900bef923846ab4741949f9bd3f885e1
kouamano <amano.au1@gmail.com>
2022-11-24 22:44:32 +0900

    rev

--- jtq_def.h
+++ jtq_def.h
@@ -1,18 +1,20 @@
 /* size */
 #define LEN 1024
 /* type */
-/** conjugator **/
-#define CjType_C 1
-#define CjType_A 2	//中間ノード ']'
-#define CjType_D 3	//中間ノード '}'
-#define CjType_AA 11
-#define CjType_AD 12
-#define CjType_DD 21 
-#define CjType_DA 22 
 /** node **/
 #define NdType_Dict 1
 #define NdType_Arr 2
 #define NdType_KV 4
 #define NdType_Key 8
 #define NdType_Val 16
-#define NdType_IM 32	//中間ノード
+#define NdType_Cj 32
+#define NdType_Cl 64
+/** conjugator **/
+// for NdType_Cj
+#define CjType_C 256	// ","
+#define CjType_D 512	// "{"
+#define CjType_A 1024	// "["
+/** close **/
+// for NdType_Cl
+#define ClType_D 4096	// "}"
+#define ClType_A 8192	// "]"
