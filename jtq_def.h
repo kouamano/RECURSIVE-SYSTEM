4d83b48ba92181e088254cfab54c00131531e3d4
kouamano <amano.au1@gmail.com>
2022-11-25 14:08:10 +0900

    add

--- jtq_def.h
+++ jtq_def.h
@@ -10,11 +10,11 @@
 #define NdType_Cj 32
 #define NdType_Cl 64
 /** conjugator **/
-// for NdType_Cj
+// for NdType_Cj	// 先行のNdにセット
 #define CjType_C 256	// ","
 #define CjType_D 512	// "{"
 #define CjType_A 1024	// "["
-/** close **/
+/** close **/		// 自身のNdにセット
 // for NdType_Cl
 #define ClType_D 4096	// "}"
 #define ClType_A 8192	// "]"
