e1bfcd95f11ad4ea82962517ba1a3db1eaaf5210
kouamano <amano.au1@gmail.com>
2023-04-16 16:59:47 +0900

    add

--- compile-all.sh
+++ compile-all.sh
@@ -2,6 +2,8 @@ gcc cts.c -o cts
 gcc cst.c -o cst
 gcc cjt.c -o cjt
 gcc iq.c -o iq
+gcc jt.c -o jt
+gcc xtq.c -o xtq
 gcc tconv.c -o tconv
 gcc -Wall -O2 -march=native tq.c -o tq.o
 gcc -Wall -g -O0 c_tq.c -o c_tq.o
