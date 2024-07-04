754edd7a5ea505f08edb3228df5618b10ecc481a
kouamano <amano.au1@gmail.com>
2024-07-04 18:49:59 +0900

    under update

--- xtq.c
+++ xtq.c
@@ -8,7 +8,7 @@
 /* help */
 void help(void){
 	printf("USAGE:\n");
-	printf(" xtq [-h] [-s] [-c] [-pt] [-pn] [-pB] [-pBS] [-pT] [-pJ] [-pJJ] [-pY] in=<input file> buff=<buffer size> blocks=<blocks> pf=<print format> BH=<block header> BF=<block footer> SH=<status header> SF=<status footer>.\n");
+	printf(" xtq [-h] [-s] [-c] [-pt] [-pn] [-pB] [-pBS] [-pBL] [-pT] [-pJ] [-pJJ] [-pY] in=<input file> buff=<buffer size> blocks=<blocks> pf=<print format> BH=<block header> BF=<block footer> SH=<status header> SF=<status footer>.\n");
 	printf("  -h : help.\n");
 	printf("  -s : status.\n");
 	printf("  -c : check args.\n");
@@ -16,6 +16,7 @@ void help(void){
 	printf("  -pn : print LF after block.\n");
 	printf("  -pB : print blocks.\n");
 	printf("  -pBS : print blocks with status.\n");
+	printf("  -pBL : print blocks with node level only.\n");
 	printf("  -pT : print blocks with T-form.\n");
 	printf("  -pJ : print blocks with JSON.\n");
 	printf("  -pJJ : print blocks with complete JSON.\n");
@@ -108,6 +109,8 @@ void get_options(int optc, char **optv, struct options *opt){
 			(*opt).pn = 1;
 		}else if(strncmp(optv[i],"-pBS",4) == 0){
 			(*opt).pbs = 1;
+		}else if(strncmp(optv[i],"-pBL",4) == 0){
+			(*opt).pbs = 2;
 		}else if(strncmp(optv[i],"-pB",3) == 0){
 			(*opt).pb = 1;
 		}else if(strncmp(optv[i],"-pT",3) == 0){
--- xtq_block.h
+++ xtq_block.h
@@ -172,3 +172,21 @@ void ExPrint_seq_BlSt(struct Block *Bl, int target, struct options opt, struct B
 		}
 	}
 }
+void ExPrint_seq_BlLv(struct Block *Bl, int target, struct options opt, struct Bl_tree_report report){
+	int i;
+	for(i=target;i<report.Bls;i++){
+		//status
+		//printf("%s",opt.SH);
+		//printf("Bl%d,Ty%d,Lv%d,Pa%d",i,Bl[i].Bltype,Bl[i].Lv,Bl[i].parent);
+		printf("Bl%d%s,%sTy%d%s,%sLv%d%s,%sPa%d%s",i,opt.SF,opt.SH,Bl[i].Bltype,opt.SF,opt.SH,Bl[i].Lv,opt.SF,opt.SH,Bl[i].parent,opt.SF);
+		//printf("%s",opt.SF);
+		//block
+		printf("%s",opt.BH);
+		printf(opt.pf,Bl[i].str);
+		printf("%s",opt.BF);
+		//tail
+		if(opt.pn == 1){
+			printf("\n");
+		}
+	}
+}
