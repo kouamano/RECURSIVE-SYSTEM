fb05e85cbb4cb725fae7f54ea1d80b97054067c9
kouamano <kouamano@kouamanonoMac-Studio.local>
2025-08-26 13:33:05 +0900

    rev

--- xtq.c
+++ xtq.c
@@ -18,8 +18,8 @@ void help(void){
 	printf("  -n : print LF after block.\n");
 	printf("  -pB : print blocks.\n");
 	printf("  -pBS : print blocks with status.\n");
-	printf("  -pBL : print blocks with node level only.\n");
-	printf("  -pBlp : print leaf blocks only, with parent.\n");
+	printf("  -pBL : print blocks with node level.\n");
+	printf("  -pBlp : print leaf blocks with parent.\n");
 	printf("  -fT : print blocks with T-form.\n");
 	printf("  -fJ : print blocks with JSON.\n");
 	printf("  -fJJ : print blocks with complete JSON.\n");
