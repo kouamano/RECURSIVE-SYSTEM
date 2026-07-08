b5a5a276ff4ff4e344cdffff4a675ae24fa57a6b
kouamano <amano.au1@gmail.com>
2026-07-08 17:23:39 +0900

    add

--- xtq.c
+++ xtq.c
@@ -17,7 +17,7 @@ void help(void){
 	printf("  -t : print terminal chars.\n");
 	printf("  -n : print LF after block.\n");
 	printf("  -pB : print blocks.\n");
-	printf("  -pBS : print blocks with status.\n");
+	printf("  -pBS : print blocks with full status.\n");
 	printf("  -pBL : print blocks only with node level.\n");
 	printf("  -pBlp : print leaf blocks with parent.\n");
 	printf("  -fT : print blocks with T-form.\n");
